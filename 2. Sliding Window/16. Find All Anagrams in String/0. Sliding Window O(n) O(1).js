e3f9fec651ffbe21d2db5c762b2646038be87274
Volatale <nathanamarirose@gmail.com>
2024-04-23 05:29:14 +0100

    Add Permutation in String & Find All Anagrams

---
+++ 2. Sliding Window/16. Find All Anagrams in String/0. Sliding Window O(n) O(1).js
@@ -0,0 +1,53 @@
+//* We are given two strings, and we need to find "p" in "s"
+//* So we know it has to be a substring searching problem
+//* Get the frequency of characters in "p", and do t he same for "s" up to p.length
+//* Then just keep a fixed window size of p.length and check for anagrams along the way
+function findAllAnagrams(s, p) {
+  if (s.length < p.length) return [];
+
+  const results = [];
+
+  //* Sliding Window
+  let start = 0;
+  let end = 0;
+
+  //* Base 26 Arrays
+  const freq1 = new Array(26).fill(0);
+  const freq2 = new Array(26).fill(0);
+
+  //* Generate the anagram frequency for "p"
+  for (let i = 0; i < p.length; i++) {
+    freq1[p[i].charCodeAt(0) - 97]++;
+  }
+
+  while (end < s.length) {
+    freq2[s[end++].charCodeAt(0) - 97]++;
+
+    //* If there are at least p.length characters in the window
+    if (end - start + 1 === p.length) {
+      if (isAnagram(freq1, freq2)) results.push(start);
+      freq2[s[start++].charCodeAt(0) - 97]--;
+    }
+  }
+
+  return results;
+}
+
+function isAnagram(arr1, arr2) {
+  for (let i = 0; i < 25; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+console.log(findAllAnagrams("cbaebabacd", "abc")); //* [0, 6]
+console.log(findAllAnagrams("abab", "ab")); //* [0, 1, 2]
+console.log(findAllAnagrams("sonic", "sonic")); //* [0]
+
+//* Time: O(n) - It takes O(p) time to iterate over "p" to get the first window
+//* Then it takes O(n) time (s.length) to iterate over s
+//* Checking for anagrams takes O(26) -> O(1) time, so constant time
+
+//* Space: O(26) -> O(1) - The input is limited to lowercase alphabet
+//* The frequency arrays will always use the same amount of space regardless of input size
---
+++ 2. Sliding Window/5. Permutation in String/1. Sliding Window O(n) O(1).js
@@ -0,0 +1,54 @@
+//* The question is essentially asking for an Anagram of s1 that exists in s2
+//* A "permutation" can be considered a different order of something
+//* Thus, a permutation and an anagram are effectively the same in this context
+function permutationInString(s1, s2) {
+  if (s2.length < s1.length) return false; //* A permutation is impossible
+
+  //* Sliding Window
+  let start = 0;
+  let end = 0;
+
+  //* Base 26 Arrays ("a" = 97. "a" - "a" = 0)
+  const freq1 = new Array(26).fill(0);
+  const freq2 = new Array(26).fill(0);
+
+  //* Generate both windows at once
+  for (let i = 0; i < s1.length; i++) {
+    freq1[s1[i].charCodeAt(0) - 97]++;
+    freq2[s2[i].charCodeAt(0) - 97]++;
+    end++;
+  }
+
+  if (isAnagram(freq1, freq2)) return true;
+
+  while (end < s2.length) {
+    freq2[s2[end++].charCodeAt(0) - 97]++;
+
+    //* Remove leftmost char from the window
+    freq2[s2[start++].charCodeAt(0) - 97]--;
+
+    if (isAnagram(freq1, freq2)) return true;
+  }
+  return false;
+}
+
+//* O(26)
+function isAnagram(arr1, arr2) {
+  for (let i = 0; i < 25; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+console.log(permutationInString("ab", "bloba")); //* true
+console.log(permutationInString("icson", "sonic")); //* true
+console.log(permutationInString("zw", "wz")); //* true
+console.log(permutationInString("w", "z")); //* false
+console.log(permutationInString("law", "lae")); //* false
+
+//* Time: O(n) - We have to process each character in both strings
+//* We also have an O(26) call in isAnagram, but this always takes the same amount of time
+
+//* Space: O(26) -> O(1) - We have two frequency arrays that always have 26 indices
+//* They do not scale in size with either input, so the function uses constant space
