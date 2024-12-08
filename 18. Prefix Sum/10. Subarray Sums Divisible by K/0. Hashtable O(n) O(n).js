b1748e87db2214a9d2527e0ea0fd24e2cbace007
Volatale <nathanamarirose@gmail.com>
2024-12-08 23:41:23 +0000

    Add more information on Subarray Sums Divisible by K

--- 18. Prefix Sum/10. Subarray Sums Divisible by K/0. Hashtable O(n) O(n).js
+++ 18. Prefix Sum/10. Subarray Sums Divisible by K/0. Hashtable O(n) O(n).js
@@ -7,6 +7,22 @@
 //*         - Then mod by k again to ensure it is within the range [0, remainder) (exclusive)
 //!     - This is necessary because in programming, we only have the % operator
 //*         - But the % operator is a REMAINDER operator, not the modulus operator that exist in mathematics
+
+//* Let psum_i represent the prefix sum up to index i
+//*     - psum_i = arr[0] + arr[1] + ... + arr[i]
+//* For any subarray starting at j and ending at i, the SUM of the subarray is:
+//*     - sum(subarray[j:i]) = psum_i - psum_j-1
+//! We want the subarray sum to be DIVISIBLE by K
+//*     - sum(subarray[j:i]) = (psum_i - psum_j-1) % K = 0
+//* Rearrange the formula such that we get
+//*     - psum_i % K = psum_j-1 % K
+//!     - The REMAINDER when psum_i or psum_j-1 are divided by K should be equal
+//* As we iterate through the array, compute the prefix sum mod k for each index
+//*     - remainder_i = psum_i % K
+//* Use a map or dictionary to track how many times each remainder has occurred thus far
+//*     - countMap[remainder]++
+//* If the current remainder_i has been seen before
+//*     - It means there are countMap[remainder] subarrays ENDING at index i that are also divisible by K
 function subarraysDivByK(nums, k) {
   const seen = { 0: 1 }; //* Mod : Frequency of subarray
   let subarrays = 0;
