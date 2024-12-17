49e842c50eda27896446c19483698772fce7582a
Volatale <nathanamarirose@gmail.com>
2024-12-17 19:02:02 +0000

    Add extra info for Count Triplets That Can Form

--- 18. Prefix Sum/16. Count Triplets That Can Form Two Arrays of Equal XOR/0. Hashtable Prefix XOR O(n) O(n).js
+++ 18. Prefix Sum/16. Count Triplets That Can Form Two Arrays of Equal XOR/0. Hashtable Prefix XOR O(n) O(n).js
@@ -19,6 +19,14 @@
 //* If "k" = 2 and "i" = 0
 //*     - Then we get (2 - 0) = 2
 //!     - Thus does NOT include the element at index i (which is correct)
+
+//* Count all triplets such that:
+//*     - XOR(i -> j - 1) = XOR(j -> k)
+//* Rewrite it as:
+//*     - prefixXOR[j-1] ^ prefixXOR[i - 1] = prefixXOR[k] ^ prefixXOR[j-1]
+//* Simplify the equation:
+//*     - prefixXOR[i-1] = prefixXOR[k]
+//!     - Why? Because prefixXOR[j-1] ^ prefixXOR[j-1] cancels out, which just leaves the other two terms
 function countTriplets(arr) {
   //* There are no possible triplets
   if (arr.length === 0) return 0;
