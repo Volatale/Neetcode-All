6df704a05986443251ee8c32e724b32db003788c
Volatale <nathanamarirose@gmail.com>
2025-02-08 23:25:58 +0000

    Add Sum of Abs Diff in a Sorted Array

---
+++ 0.5. Array and Hashing II/26. Sum of Abs Diff in Sorted Array/1. Brute Force O(n^2) O(1).js
@@ -0,0 +1,28 @@
+//* Try every pair of elements
+//* Skip the current iteration if i === j
+function getSumAbsoluteDifferences(nums) {
+  //* result[i] = abs diff between nums[i] and all other elements
+  const result = new Array(nums.length).fill(0);
+
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+
+    for (let j = 0; j < nums.length; j++) {
+      //* Ignore identical indices
+      if (i === j) continue;
+
+      sum += Math.abs(nums[i] - nums[j]);
+    }
+
+    result[i] = sum;
+  }
+
+  return result;
+}
+
+console.log(getSumAbsoluteDifferences([2, 3, 5])); //* [4, 3, 5]
+console.log(getSumAbsoluteDifferences([1, 4, 6, 8, 10])); //* [24, 15, 13, 15, 21]
+
+//* Time: O(n^2) - The number of iterations we do scales with n * (n + 1) / 2
+
+//* Space: O(1) - The memory usage remains constant regardless of input size
