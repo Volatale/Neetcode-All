18268d737b8fbd73e6e69579e5bc5c460a6b0961
volatale <nathanamarirose@gmail.com>
2025-03-19 22:25:27 +0000

    Add Min Ops To Make Binary Array Equal to One

--- 12. 1D Dynamic Programming/4. Longest Palindromic Substring/0. Expand From Center O(n^2) O(n).js
+++ 12. 1D Dynamic Programming/4. Longest Palindromic Substring/0. Expand From Center O(n^2) O(n).js
@@ -1,18 +1,19 @@
 //* Palindromes are naturally symmetrical
-//* So we can expand OUTWARD from the current character
-//*     - That is, instead of going from the outside in
+//* So instead of generating every substring and checking if its a palindrome
+//* We can expand OUTWARD from the current character
 //* Use the two pointer technique to handle comparing two characters simultaneously
 //* Palindromes can be both odd and even length
 //*     - Even palindromes start at (i, i + 1) for left and right respectively
 //!         - These handle cases like "abba" and "abb" (they even handle some odd palindromes)
 //*     - Odd palindromes start at (i, i) for both left and right
+//* Essentially, the substring is created using characters BETWEEN left and right (exclusive)
 function longestPalindrome(s) {
-  //* Base Case
   if (s.length === 1) return s[0];
 
+  //* The minimum length a palindrome can be is 1 (a single character)
   let longest = s[0];
 
-  //* Expand from every index to get every possible palindrome
+  //* Expand OUTWARD from every index in both directions to get all possible palindromes
   for (let i = 0; i < s.length - 1; i++) {
     const odd = expandFromCenter(s, i, i); //* Odd length palindrome
     const even = expandFromCenter(s, i, i + 1); //* Even length palindrome
@@ -48,7 +49,7 @@ console.log(longestPalindrome("xay")); //* "a", "x" or "y"
 console.log(longestPalindrome("abb")); //* "bb"
 
 //* Time: O(n^2) - We use an O(n) for loop to get every index i from 0 to n - 1
-//* Within each for loop, we perform an O(n) loop (in the worst case) to expand from the center
+//* Within each iteration, we perform an O(n) loop (in the worst case) to expand from the center
 
 //* Space: O(n) or O(1) - We create a string of length "n" in the worst case
 //* Imagine the entire string is a palindrome, then we'd return a string of length "n"
---
+++ 14.5. Greedy II/19. Min Ops To Make Binary Array Equal 1/0. Greedy O(n) O(1).js
@@ -0,0 +1,40 @@
+//* In a greedy manner, whenever we encounter a 0
+//*     - Flip the current triplet
+//* Ultimately, we have to flip each group as they come
+//* Use the Bitwise XOR operator to handle that for us
+//*     - nums[i] ^= 1
+//*     - nums[i + 1] ^= 1
+//*     - nums[i + 2] ^= 1
+//! The array consists of all 1s if:
+//*     - nums[n-1] and nums[n-2] are both 1
+//* Why? Any valid solution leaves the last two elements as 1
+//*     - If they are NOT both 1, then non solution exists
+
+function minOperations(nums) {
+  const n = nums.length;
+  let flips = 0;
+
+  //* If we encounter a 0, flip the current triplet
+  for (let i = 0; i < n - 2; i++) {
+    if (nums[i] === 0) {
+      nums[i] ^= 1;
+      nums[i + 1] ^= 1;
+      nums[i + 2] ^= 1;
+      flips++;
+    }
+  }
+
+  //* If the last two elements are 1, we have a valid solution
+  return nums[n - 1] === 1 && nums[n - 2] === 1 ? flips : -1;
+}
+
+console.log(minOperations([0, 0, 0])); //* 1
+console.log(minOperations([1, 1, 1])); //* 0
+console.log(minOperations([1, 1, 1, 0, 0, 0])); //* 1
+console.log(minOperations([0, 1, 0])); //* -1
+console.log(minOperations([0, 1, 1, 1])); //* -1
+console.log(minOperations([0, 1, 1, 1, 0, 0])); //* -1
+
+//* Time: O(n) - The time taken scales with the size of the input
+
+//* Space: O(1) - The memory usage remains constant regardless of input size
