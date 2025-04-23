de98116a2a45a8feeaf05e957dbcfe621689e265
volatale <nathanamarirose@gmail.com>
2025-04-23 10:36:24 +0100

    Update Replace Elements with Greater

--- 0. Array and Hashing/0 - 10/3. Replace Elements with Greater/0. Cached O(n) O(1).js
+++ 0. Array and Hashing/0 - 10/3. Replace Elements with Greater/0. Cached O(n) O(1).js
@@ -1,14 +1,21 @@
-//* Initial max = -1, because it eliminates edge cases
-//* Reverse iteration
-//* new max = max(oldMax, arr[i])
+//* The goal is to replace every element of the array with the greatest element to its right
+//* Ultimately, we need to iterate from right to left
+//* Once the algorithm is (correctly) performed, we end up with a monotonically non-increasing array
+//* So that indicates that from left to right, the values are as follows:
+//*     - For all i in the range 0 <= i < n, nums[i] >= nums[i + 1]
+//* So simply track the maximum value going from right to left
+//* Then set nums[i] equal to that value
 
+//* For each element, record the current value (arr[i]), and overwrite it (arr[i]) using the last maximum
+//* Then, potentially update the maximum using the new value
 function replaceElements(arr) {
-  let rightmax = -1;
+  //* The final element should be -1
+  let max = -1;
 
   for (let i = arr.length - 1; i >= 0; i--) {
-    let newMax = Math.max(rightmax, arr[i]);
-    arr[i] = rightmax;
-    rightmax = newMax;
+    const curr = arr[i];
+    arr[i] = max;
+    max = Math.max(max, curr);
   }
 
   return arr;
@@ -16,7 +23,8 @@ function replaceElements(arr) {
 
 console.log(replaceElements([17, 18, 5, 4, 6, 1]));
 console.log(replaceElements([400]));
+console.log(replaceElements([1, 3, 2, 5, 6, 1, 4, 6, 7, 1, 9]));
 
-//* Time: O(n) - We only do one pass through the array, so the time taken scales linearly with the input length
+//* Time: O(n) - The time taken scales with the input size since we iterate through every element
 
-//* Space: O(1) - We don't use any extra auxillary space at all; the space remains constant
+//* Space: O(1) - The memory usage remains constant regardless of input size
--- 0. Array and Hashing/0 - 10/3. Replace Elements with Greater/1. Nested Loops O(n^2) O(1).js
+++ 0. Array and Hashing/0 - 10/3. Replace Elements with Greater/1. Nested Loops O(n^2) O(1).js
@@ -1,22 +1,28 @@
-//* The "largest" checks should only start at i + 1 (it doesn't have to be > arr[i])
+//* The goal is to replace every element of the array with the greatest element to its right
+//* So in the most brute force manner, for every element, find the largest element to the right
+//*     - Replace the current element with that value
 function replaceElements(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let largest = arr[i + 1]; // 17
+  //* For each element arr[i]
+  for (let i = 0; i < arr.length - 1; i++) {
+    let max = -1;
 
+    //* Find the largest element to the right of "i"
     for (let j = i + 1; j < arr.length; j++) {
-      largest = Math.max(largest, arr[j]);
+      max = Math.max(max, arr[j]);
     }
 
-    arr[i] = largest;
+    arr[i] = max;
   }
 
+  //* The last element becomes -1
   arr[arr.length - 1] = -1;
+
   return arr;
 }
 
 console.log(replaceElements([17, 18, 5, 4, 6, 1]));
 console.log(replaceElements([400]));
 
-//* Time: O(n^2) - We have two nested for loops that both scale with the length of the input array
+//* Time: O(n^2) - We have a pair of nested for loops, both of which scale with the input size (n)
 
-//* Space: O(1) - We don't use any extra auxillary space at all; the space remains constant
+//* Space: O(1) - The memory usage remains constant regardless of input size
