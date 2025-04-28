be5428392e2bb3a1105cc021d2220d773d520e4f
volatale <nathanamarirose@gmail.com>
2025-04-28 09:44:31 +0100

    Update Unique Email Addresses

--- 0. Array and Hashing/11 - 20/11. Unique Email Addresses/0. Split O(n m) O(n).js
+++ 0. Array and Hashing/11 - 20/11. Unique Email Addresses/0. Split O(n m) O(n).js
@@ -1,48 +1,51 @@
-//* Use a set that stores strings to count the number of unique emails
-//* A set automatically filters duplicates
-//* Iterate over each individual email
-//* Split the email into everything before the "@" and after (local vs domain)
-//* Further split the local via a "+", this allows us to ignore everything AFTER the +
-//* Replace every instance of "." with "" to ignore the ".", since we want to ignore those too
-//* Add the unique string to the set
-function uniqueEmails(emails) {
+//* Valid emails consist of a "local" name and a "domain" name
+//* In the email "sonic@greenhill.com":
+//*     - "sonic" is the local name
+//*     - "greenhill.com" is the domain name
+//! If you add a periods (".") between characters in the local name:
+//*     - mail sent there will be FORWARDED to the same address WITHOUT the dot
+//*     - "sonic.g" and "sonicg" receive the SAME emails, in other words
+//! If you add a plus ("+") between characters in the local name:
+//*     - Anything AFTER the plus is IGNORED
+//*     - An email sent to "sonic+knuckles@greenhill.com" will ACTUALLY be sent to "sonic@greenhill.com"
+//! It is possible to use both rules at once (in the same email)
+
+//* We need to return the number of different (unique) email addresses that "actually" receive emails
+//* A (hash)set or a (hash)map work perfectly for this case
+//*     - Neither will allow duplicates
+//*     - The number of emails that actually get sent is equivalent to the size
+//* Determine what email we're "actually" sending mail to, and then add that to the data structure
+function numUniqueEmails(emails) {
+  //* Tracks the emails that have actually received mail
   const unique = new Set();
 
   for (let email of emails) {
+    //* The local and domain names are split via an "@"
     let [local, domain] = email.split("@");
 
-    //* Take the first string ("test.email+alex".split("+") gives ["test.email", "alex")
-    //* We "ignore" everything after the "+" by not using the value
+    //* "." are essentially ignored in our case, so remove all of the "."
+    //* And in the case of "+", remove everything AFTER the "+"
+    local = local.replaceAll(".", "");
     local = local.split("+")[0];
-    local = local.replaceAll(".", ""); //* Erase every occurrence of ".", because we can ignore it
 
+    //* Keep track of the (processed) email
     unique.add(`${local}-${domain}`);
   }
 
+  //* unique.size === no. of (unique) emails that received mail
   return unique.size;
 }
 
-console.log(
-  uniqueEmails([
-    "test.email+alex@leetcode.com", // testemail@leetcode.com
-  ])
-); // 1
+console.log(numUniqueEmails(["test.email+alex@leetcode.com"])); //* 1
 
 console.log(
-  uniqueEmails([
+  numUniqueEmails([
     "test.email+alex@leetcode.com", // testemail@leetcode.com
     "test.e.mail+bob.cathy@leetcode.com", // testemail@leetcode.com
-    "testemail+david@lee.tcode.com", // testemail@lee.tcode.com //! Unique because neither rule applies to domain names
+    "testemail+david@lee.tcode.com", // testemail@lee.tcode.com
   ])
-); // 2
+); //* 2
 
 console.log(
-  uniqueEmails(["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"])
-); // 3
-
-//* Time: O(n * m) - It takes O(n) time to iterate through each individual email
-//* It takes O(m) time to split, and we do that twice
-//* Then it takes O(m) to replace all the occurrences of "." with ""
-//* Its constant time to add the string to the unique set
-
-//* Space: O(n) - The set's size scales with the number of UNIQUE email addresses post-processing
+  numUniqueEmails(["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"])
+); //* 3
