717ccfa79d8f6649f47f851533662598a1ca43f5
volatale <nathanamarirose@gmail.com>
2025-05-06 14:01:30 +0100

    Update Find Disappeared Numbers

--- 0. Array and Hashing/11 - 20/18. Find Disappeared Numbers/0. Cyclic Sort O(n) O(1).js
+++ 0. Array and Hashing/11 - 20/18. Find Disappeared Numbers/0. Cyclic Sort O(n) O(1).js
@@ -1,35 +1,53 @@
-//* Each value should be stored at index "value - 1"
-//* So if the value is 1, it should be stored at [1 - 1], which is index 0
-//* Then, just iterate through the array again
-//* If "i" !== nums[i - 1], that means it is out of place; add it to to results
+//* We are given an array of (n) integers whose values are between [1, n]
+//* And we need to return an array of all of the integers that do not appear in the array
+//* If "n" = 3, for example, any of the following arrays are valid:
+//*     - [1, 2, 3]
+//*     - [3, 3, 3]
+//*     - [1, 1, 2]
+//*     - etc.
+//* The array may also contain duplicates
+//* Since we know all of the elements inn the array are in the range [1, n]
+//*     - This implicitly means that the array contains no negative numbers
+//*     - And it also means the array doesn't contain numbers > n, nor does it contain 0
+//* Thus, we can use Cyclic Sort to rearrange the elements in the array
+//* After doing so, all of the non-duplicate elements will be in the first "k" indices
+//*    - Where "k" is the number of unique elements in the array
+//* Then, we can simply iterate through and check if the value at index "i" = i + 1
+//*     - If it is not, then we know the value "i + 1" is missing
 function findDisappearedNumbers(nums) {
-  const result = [];
+  //* There are no missing numbers
+  if (nums.length === 0) return [];
 
-  //* Put each element in the correct index
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
+  const missingNumbers = [];
+  let i = 0;
 
-    //* While index num-1 does not store the correct value
-    while (nums[num - 1] !== num) {
-      [nums[num - 1], num] = [num, nums[num - 1]]; //* Swap
+  //* Perform Cyclic Sort
+  while (i < nums.length) {
+    const correctIndex = nums[i] - 1; //* Elements are in the range [1, n]; subtract 1
+
+    if (nums[i] !== nums[correctIndex]) {
+      [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+    } else {
+      i++;
     }
   }
 
-  //* Find the values that are NOT in place
-  for (let i = 1; i <= nums.length; i++) {
-    if (nums[i - 1] !== i) result.push(i);
+  //* Find all of the missing numbers (numbers that do not exist in the set)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      missingNumbers.push(i + 1);
+    }
   }
 
-  return result;
+  return missingNumbers;
 }
 
-console.log(findDisappearedNumbers([1, 4, 4, 2])); // [3]
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5, 6]
-console.log(findDisappearedNumbers([1, 1])); // [2]
+console.log(findDisappearedNumbers([1, 4, 4, 2])); //* [3]
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); //* [5, 6]
+console.log(findDisappearedNumbers([1, 1])); //* [2]
+console.log(findDisappearedNumbers([1, 2, 3])); //* []
 
-//* Time: O(n) - The outer loop takes O(n) time since we iterate through the whole array
-//* The while loop scales with "n" too, but it only processes each element once in total
-//* Then we have another for loop that once again scales with n
-//* So O(3n) simplifies to O(n)
+//* Time: O(n) - We iterate through the input array, and create the set
 
-//* Space: O(1) - We don't use any extra space, as the result array is said to be constant in this question
+//* Space: O(1) - Cyclic sort allows us to sort the array in place
+//* Thus, the memory usage remains constant regardless of input size
--- 0. Array and Hashing/11 - 20/18. Find Disappeared Numbers/1. Set O(n) O(n).js
+++ 0. Array and Hashing/11 - 20/18. Find Disappeared Numbers/1. Set O(n) O(n).js
@@ -1,30 +1,37 @@
-//* Use a set (they have fast lookup - Θ(1) in JS)
-//* Iterate from 1 to nums.length (inclusive)
-//* If we DON'T find "i" within set, that means it is not in the array
-//* Therefore we need to add it to the results array
+//* We are given an array of (n) integers whose values are between [1, n]
+//* And we need to return an array of all of the integers that do not appear in the array
+//* If "n" = 3, for example, any of the following arrays are valid:
+//*     - [1, 2, 3]
+//*     - [3, 3, 3]
+//*     - [1, 1, 2]
+//*     - etc.
+//* The array may also contain duplicates
+//* We can iterate through the array and store the unique values in a set
+//* Then, we can iterate through the range [1, n] (inclusive) and check if the set contains the value
+//* If it does not, then the current value is missing
 function findDisappearedNumbers(nums) {
-  const results = [];
-  const set = new Set(nums);
+  //* There are no missing numbers
+  if (nums.length === 0) return [];
 
+  const missingNumbers = [];
+  const uniqueNumbers = new Set(nums);
+
+  //* Find all of the missing numbers (numbers that do not exist in the set)
   for (let i = 1; i <= nums.length; i++) {
-    if (!set.has(i)) {
-      results.push(i);
+    if (!uniqueNumbers.has(i)) {
+      missingNumbers.push(i);
     }
-
-    set.delete(i); //* Optimization to reduce memory usage over time
   }
 
-  return results;
+  return missingNumbers;
 }
 
-console.log(findDisappearedNumbers([1, 4, 4, 2])); // [3]
-console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); // [5, 6]
-console.log(findDisappearedNumbers([1, 1])); // [2]
+console.log(findDisappearedNumbers([1, 4, 4, 2])); //* [3]
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])); //* [5, 6]
+console.log(findDisappearedNumbers([1, 1])); //* [2]
+console.log(findDisappearedNumbers([1, 2, 3])); //* []
 
-//* Time: O(n) - We initialize the set using the nums array, which takes O(n)
-//* Then, we iterate from 1 up to (and including) nums.length (n)
-//* So in reality, it is O(2n), but Big O Notation simplifies to O(n)
+//* Time: O(n) - We iterate through the input array, and create the set
 
-//* Space: O(n) - The set initializes with the same number of elements as the input in the worst case
-//* If every element in nums is unique, that means the set will also have the same amount of elements
-//* In the case where every element in nums is a duplicate, the set would only have a size of 1
+//* Space: O(n) - In the worst case, the set's size is equal to the input array
+//* This occurs when all of the elements in the input array are unique
