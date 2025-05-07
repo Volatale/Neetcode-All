3f8f6343a058ff34a843e12f0f52d2d9df3b9654
volatale <nathanamarirose@gmail.com>
2025-05-08 00:32:46 +0100

    Update Word Pattern

--- 0. Array and Hashing/11 - 20/20. Word Pattern/0. Isomorphic Strings & Split O(n + m) O(m).js
+++ 0. Array and Hashing/11 - 20/20. Word Pattern/0. Isomorphic Strings & Split O(n + m) O(m).js
@@ -1,50 +1,55 @@
-//* Remove the whitespace between each word using split
-//* Create a map for the pattern, and for the string
-//* Check for bi-directional isomorphism
-//! JS goes up the prototype chain for keys if it doesn't find it on the current object
-//! So we need to make sure we only check if and only if the keys exist on THESE objects
-//! We can't just do map1[word] if "word" is "constructor", for example; constructor exists as a key upstream
-//! Make sure that we check for enumerable OWN keys
-//* Eventually there will be a mismatch in word vs char if they are not isomorphic
+//* Given a string pattern and string s, we need to check if s follows the same pattern
+//* In this case "follow" means a full match:
+//*     - Each letter in pattern maps to exactly one unique word in s
+//*     - Each unique word in s maps to exactly one letter in pattern
+//*     - No two letters map the same word, and no two words map to the same letter
+//* In other words, we need to perform a two-way mapping between letters in pattern and words in s
+//* Use maps to store the mapping between letters and words
+//* This can be reduced to checking if the two strings are isomorphic
+//*     - In other words, checking if they have the same structure
+//*     - For example, "egg" and "add" are isomorphic, but "foo" and "bar" are not
 function wordPattern(pattern, s) {
+  //* Get all of the words in s
   const words = s.split(" ");
 
+  //* It is impossible for the two strings to be isomorphic
   if (pattern.length !== words.length) return false;
 
-  const map1 = {}; //* For the pattern
-  const map2 = {}; //* For the string
+  //* These maps are used to track the mappings
+  const sToP = {};
+  const pToS = {};
 
   for (let i = 0; i < pattern.length; i++) {
     const char = pattern[i];
     const word = words[i];
 
+    //* The character/word is already mapped to something else
     if (
-      (map1.hasOwnProperty(char) && map1[char] !== word) ||
-      (map2.hasOwnProperty(word) && map2[word] !== char)
+      (sToP.hasOwnProperty(char) && sToP[char] !== word) ||
+      (pToS.hasOwnProperty(word) && pToS[word] !== char)
     ) {
       return false;
     }
 
-    map1[char] = word;
-    map2[word] = char;
+    //* Create the mapping between character and word
+    sToP[char] = word;
+    pToS[word] = char;
   }
 
+  //* The two strings are isomorphic
   return true;
 }
 
-console.log(wordPattern("ab", "dog cat")); // true
-console.log(wordPattern("abba", "dog cat cat dog")); // true
-console.log(wordPattern("abba", "dog cat cat fish")); // false
-console.log(wordPattern("aaaa", "dog cat cat dog")); // false
-console.log(wordPattern("abba", "dog dog dog dog")); // false
-console.log(wordPattern("aaaa", "dog dog dog dog")); // true
-console.log(wordPattern("abba", "dog constructor constructor dog")); // true
+console.log(wordPattern("ab", "dog cat")); //* True
+console.log(wordPattern("abba", "dog cat cat dog")); //* True
+console.log(wordPattern("abba", "dog cat cat fish")); //* False
+console.log(wordPattern("aaaa", "dog cat cat dog")); //* False
+console.log(wordPattern("abba", "dog dog dog dog")); //* False
+console.log(wordPattern("aaaa", "dog dog dog dog")); //* True
+console.log(wordPattern("abba", "dog constructor constructor dog")); //* True
 
-//* Time: O(n + m) - It takes O(m) time to iterate through "s" to perform the split()
-//* Then it takes O(n) time to iterate through "pattern"
-//* Both have to be done and can't be avoided, so O(n + m)
+//* Time: O(n + m) - In the worst case, "s" and "pattern" are not the same length
+//* Thus, it takes O(m) to split "s" into words, and O(n) to iterate through pattern
 
-//* Space: O(min(n, m)) - In the worst case, every all characters in the pattern are unique, and so are the words
-//* The space is mainly used by the two maps we use to form a bijection between the pattern and words
-//* We create an array (using split) that scales in size with the length of "s" - O(m)
-//* O(min(n, m)) dominates O(m)
+//* Space: O(n) - The memory used by the two maps is O(n) since by that point we know the have equal length
+//* The memory used by the split() function is O(m), but m <= n
