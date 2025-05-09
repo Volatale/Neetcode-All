6e1c03c89e69cee8d7fcabfedb0e39c6d71ed3ee
volatale <nathanamarirose@gmail.com>
2025-05-10 00:01:15 +0100

    Update Valid Palindrome II

--- 1. Two Pointers/0 - 10/1. Valid Palindrome II/0. Two Pointers O(n) .js
+++ 1. Two Pointers/0 - 10/1. Valid Palindrome II/0. Two Pointers O(n) .js
@@ -1,48 +1,56 @@
-//* Two Pointers to check two characters at once
-//* Iterate until we find characters that DO NOT match
-//* Try skipping the current character on the left
-//* If that doesn't work, try skipping the character on the right
-//* If either works, then return true because a valid palindrome exists
-//* If BOTH fail, return false because if outer characters can't be equal
-//*     - The entire string is not a palindrome
-function validPalindromeII(s) {
-  //* Two Pointers - We need to check both sides at once
+//* We need to check if "s" can become a palindrome after deleting AT MOST one character
+//*     - Which means the string could already be a palindrome (without a deletion)
+//* In this case, instead of deleting, we can simply skip a character
+//* So for each character, try skipping it and seeing if it would result in a palindrome
+//* To check for a palindrome, we can simply use a two pointer approach
+//*     - Each of the two pointers is initialized to opposite sides of the input (0 and n - 1)
+//*     - If s[left] !== s[right], then we know "s" is not a palindrome
+//* Except in our case, we only need to skip characters that we know don't match
+//* So iterate from both ends simultaneously until s[left] !== s[right]
+//*     - Then, try checking for a palindrome from "left + 1"
+//*     - And also try checking for a palindrome from "right - 1"
+//! If neither skipping the current character from EITHER perspective works
+//*     - Then we know that "s" cannot possibly be a palindrome
+function validPalindrome(s) {
+  //* Both pointers are initialized to opposite ends
   let left = 0;
   let right = s.length - 1;
 
   while (left <= right) {
     if (s[left] !== s[right]) {
+      //* We need to skip the current character from either perspective
       return (
-        isPalindrome(s, left + 1, right) || //* Try skipping left char
-        isPalindrome(s, left, right - 1) //* Try skipping right char
+        isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1)
       );
     }
 
-    //* Progress the window
+    //* The characters matched, so move to the next
     left++;
     right--;
   }
 
+  //* "s" is a palindrome
   return true;
 }
 
-//* O(n)
 function isPalindrome(s, left, right) {
   while (left <= right) {
-    if (s[left++] !== s[right--]) return false;
+    if (s[left++] !== s[right--]) {
+      return false;
+    }
   }
 
+  //* Found a palindrome
   return true;
 }
 
-console.log(validPalindromeII("aba")); //* True
-console.log(validPalindromeII("abca")); //* True
-console.log(validPalindromeII("abc")); //* False
-console.log(validPalindromeII("a")); //* True
-console.log(validPalindromeII("ac")); //* False
+console.log(validPalindrome("aba")); //* True
+console.log(validPalindrome("abca")); //* True
+console.log(validPalindrome("abc")); //* False
+console.log(validPalindrome("a")); //* True
+console.log(validPalindrome("ac")); //* True
 
-//* Time: O(n) - The time taken to iterate through every character scales with "n"
-//* It takes O(n) to check for a palindrome in the worst case
-//* We call isPalindrome TWICE at most (not once for each invalid match)
+//* Time: O(n) - It takes O(n) to check if a string is palindromic
+//* If two characters don't match, at most, we process each character twice
 
-//* Space: O(1) - We use no extra space other than two constant space variables
+//* Space: O(1) - The memory usage remains constant regardless of input size
