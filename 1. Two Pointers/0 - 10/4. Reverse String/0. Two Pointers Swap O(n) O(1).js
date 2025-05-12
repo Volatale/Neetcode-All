7a80411f4e68eb6cf2799d66ee8bda6f3fa86d45
volatale <nathanamarirose@gmail.com>
2025-05-13 00:29:00 +0100

    Update Reverse String

--- 1. Two Pointers/0 - 10/4. Reverse String/0. Two Pointers Swap O(n) O(1).js
+++ 1. Two Pointers/0 - 10/4. Reverse String/0. Two Pointers Swap O(n) O(1).js
@@ -1,17 +1,26 @@
-//* Use two pointers since we need to swap two elements at once
+//* We need to reverse an input string (in the form of an array)
+//* Reversing the string via a method is probably the most method
+//* However, we can also simply use two pointers for a more algorithmic approach
+//* Both pointers initialize to opposite ends of the input array
+//* Then, simply swap(s[left], s[right]) in place
+//* After the swap, increment left and decrement left
+//*     - Every index before "left" is reversed
+//*     - Every index after "right" is also reversed
 function reverseString(s) {
+  //* These pointers mark the next characters to swap
   let left = 0;
   let right = s.length - 1;
 
+  //* Swap each character in-place
   while (left < right) {
-    swap(s, left++, right--);
+    swap(s, left, right);
+    left++;
+    right--;
   }
-
-  return s;
 }
 
 function swap(s, left, right) {
-  let temp = s[left];
+  const temp = s[left];
   s[left] = s[right];
   s[right] = temp;
 }
@@ -20,8 +29,6 @@ console.log(reverseString(["h", "e", "l", "l", "o"])); //* ["o", "l","l","e","h"
 console.log(reverseString(["H", "a", "n", "n", "a", "h"])); //* ["o", "l","l","e","h"]
 console.log(reverseString(["a", "b", "c"])); //* ["c", "b", "a"]
 
-//* Time: O(n) - It takes O(n / 2) time to iterate over the whole input
-//* But we drop constants in Big O Notation, so it simplifies to O(n)
+//* Time: O(n) - There are "n" characters in the input, so it takes O(n) to reverse them
 
-//* Space: O(1) - The space we use remains constant
-//* It does not scale with the input size
+//* Space: O(1) - The memory usage remains constant - reverse is an inplace algorithm
--- 1. Two Pointers/0 - 10/4. Reverse String/1. Reverse O(n) O(1).js
+++ 1. Two Pointers/0 - 10/4. Reverse String/1. Reverse O(n) O(1).js
@@ -1,4 +1,5 @@
-//* Just reverse the input array
+//* We need to reverse an input string (in the form of an array)
+//* The most brute solution would be to simply call reverse()
 function reverseString(s) {
   return s.reverse();
 }
@@ -7,7 +8,6 @@ console.log(reverseString(["h", "e", "l", "l", "o"])); //* ["o", "l","l","e","h"
 console.log(reverseString(["H", "a", "n", "n", "a", "h"])); //* ["o", "l","l","e","h"]
 console.log(reverseString(["a", "b", "c"])); //* ["c", "b", "a"]
 
-//* Time: O(n) - It takes O(n) time to reverse the input
+//* Time: O(n) - There are "n" characters in the input, so it takes O(n) to reverse them
 
-//* Space: O(1) - The space we use remains constant
-//* It does not scale with the input size
+//* Space: O(1) - The memory usage remains constant - reverse is an inplace algorithm
