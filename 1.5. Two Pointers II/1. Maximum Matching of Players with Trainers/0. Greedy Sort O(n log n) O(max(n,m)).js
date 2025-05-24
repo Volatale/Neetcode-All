93fdc1e828e886586366d4ba0cfbf116e7dd6397
volatale <nathanamarirose@gmail.com>
2025-05-25 00:53:05 +0100

    Update Max Matching of Players with Trainers

--- 1.5. Two Pointers II/1. Maximum Matching of Players with Trainers/0. Greedy Sort O(n log n) O(max(n,m)).js
+++ 1.5. Two Pointers II/1. Maximum Matching of Players with Trainers/0. Greedy Sort O(n log n) O(max(n,m)).js
@@ -1,25 +1,37 @@
-//* Apply a greedy approach
-//* We want to MAXIMIZE the amount of matches possible
-//* So realistically, we should MINIMIZE the difference between player[i] and trainer[j]
-//* In other words, if we SORT the arrays in ascending order
-//* If a match DOES happen, we know we're matching with the lowest possible candidate
-//* Which means the later matches have the highest chance to be possible
+//* We are given an array of players where players[i] represents the ability of the ith player
+//* We are also given an array of trainers where trainers[j] represents the training capacity of the jth trainer
+//* The goal is to match players with trainers, but we can only match them if the player's ability is <= the trainer's capacity
+//* Additionally, each player can only be matched with ONE trainer
+//* The goal is to MAXIMIZE the number of matchings between players and trainers
+//*     - In other words, we want to optimize our player-to-trainer pairings
+//! The key here is that we want to minimize the training capacity at every given point
+//*     - We don't want to match a player with a trainer that far exceeds their ability
+//* So logically speaking, we should sort BOTH the players AND the trainers
+//* This allows us to start with the minimum-ability player AND the trainer who is the best fit
+//! By giving each player the minimum trainer necessary, we increase the chance that LATER players can be matched
+//* We are applying a greedy approach, and using two pointers to track the next player/trainer pairing
+//* There are only two cases that can occur:
+//*     - Player accepts the trainer, in which case increment both pointers (each player gets ONE trainer)
+//*     - Player does NOT accept the trainer, increment the trainer pointer to find a better trainer
+//! Since the array exhibits monotonicity (it is sorted), we can potentially find a better trainer to the right of this one
 function matchPlayersAndTrainers(players, trainers) {
-  let matched = 0;
+  //* There are no cookies to hand out
+  if (players.length === 0) return 0;
 
-  //* Sort the arrays to ensure we match with the MINIMUM necessary
+  //* Sorting in ascending order ensures we always assign the MINIMUM-ability trainer necessary
   players.sort((a, b) => a - b);
   trainers.sort((a, b) => a - b);
 
-  let i = 0; //* Tracks current player
-  let j = 0; //* Tracks current trainer
+  let matched = 0;
+  let i = 0; //* Track next playeer
+  let j = 0; //* Tracks next trainer
 
   while (i < players.length && j < trainers.length) {
     if (players[i] <= trainers[j]) {
-      matched++; //* Successfully matched player and trainer
+      matched++; //* Player accepts trainer
       i++, j++;
     } else {
-      j++; //* Need a better trainer
+      j++; //* We need a better trainer
     }
   }
 
@@ -31,8 +43,7 @@ console.log(matchPlayersAndTrainers([1, 1, 1], [10])); //* 1
 console.log(matchPlayersAndTrainers([4, 2, 3], [5, 8, 4])); //* 3
 console.log(matchPlayersAndTrainers([6, 6, 5, 2], [1, 1, 1])); //* 0
 
-//* Time: O(n log n + m log m) - We have to sort the arrays to ensure greedy works
-//* Then, iterating over the arrays takes O(max(n, m))
+//* Time: O(n log n + m log m)) - Both input arrays can have a different size
+//* Thus, the time needed scales with the time needed to sort both arrays
 
-//* Space: O(max(n, m)) - The built-in sorting array probably uses merge sort
-//* So the memory usage scales with the length of the larger of the two arrays
+//* Space: O(sort) - The memory usage scales with the sorting algorithm used
