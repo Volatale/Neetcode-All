c48d23712c12c3812421c21d353737188489b066
volatale <nathanamarirose@gmail.com>
2025-05-26 00:19:52 +0100

    Find First Palindromic String

--- 1.5. Two Pointers II/2. Find First Palindromic String/0. Brute Force O(n m) O(1).js
+++ 1.5. Two Pointers II/2. Find First Palindromic String/0. Brute Force O(n m) O(1).js
@@ -1,21 +1,25 @@
-//* Use a two pointer approach to check whether each word is a palindrome
-//* We have no choice but to brute force
+//* We need to return the first palindromic string in a string[]
+//* The "first" implies that we should iterate through the array from left-to-right
+//* We can check if a string is palindromic using the two pointer technique
+//* Simply iterate from both ends of the string at once
+//*     - If both are equal, move both pointers inwards
+//*     - Otherwise, the string is definitely not a palindrome
 function firstPalindrome(words) {
-  for (let i = 0; i < words.length; i++) {
-    if (isPalindrome(words[i])) return words[i];
+  //* There are no words, so therefore there are no palindromic strings
+  if (words.length === 0) return "";
+
+  //* Perform a palindrome check on every string in the array
+  for (let word of words) {
+    if (isPalindrome(word)) return word;
   }
 
-  //* Assume no palindrome exists
+  //* The array does not contain a palindrome
   return "";
 }
 
 function isPalindrome(word) {
-  let left = 0;
-  let right = word.length - 1;
-
-  while (left < right) {
-    if (word[left] !== word[right]) return false;
-    left++, right--;
+  for (let i = 0, j = word.length - 1; i < j; i++, j--) {
+    if (word[i] !== word[j]) return false;
   }
 
   return true;
@@ -25,6 +29,8 @@ console.log(firstPalindrome(["abc", "car", "ada", "racecar", "cool"])); //* "ada
 console.log(firstPalindrome(["notapalindrome", "racecar"])); //* "racecar"
 console.log(firstPalindrome(["def", "ghi"])); //* ""
 
-//* Time: O(n * m) - The length of words is "n" and the length of the longest word is "m"
+//* Time: O(n m) - The input array has length `n`
+//* And the time taken to check for a palindrome scales with the length of the string
+//* We take `m` to be the length of the longest string
 
-//* Space: O(1) - The memory used remains constant regardless of input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
