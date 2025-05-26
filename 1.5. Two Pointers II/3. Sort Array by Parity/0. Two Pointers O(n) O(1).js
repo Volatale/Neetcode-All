58f08d421c3300b5ebbe0a682c122b19724d9162
volatale <nathanamarirose@gmail.com>
2025-05-27 00:02:26 +0100

    Update Sort Array by Parity

--- 1.5. Two Pointers II/3. Sort Array by Parity/0. Two Pointers O(n) O(1).js
+++ 1.5. Two Pointers II/3. Sort Array by Parity/0. Two Pointers O(n) O(1).js
@@ -1,34 +1,39 @@
-//* Use two pointers
-//*     - "left" is the index of the next EVEN
-//*     - "right" is the index of the next ODD
-//* If nums[left] is already even, move on
-//* Else if nums[right] is already odd, move on
-//* Else, the elements at [left] and [right] are out of place -> swap them
+//* We are given an int[] and we need to move all of the EVEN integers to the start
+//* Inadvertently, this also has a side effect of moving all of the ODD integers to the end
+//* Since we need to move all the even elements to the front of the array
+//* We can apply a two pointer approach, where we specifically search for even elements
+//* One of the pointers can be used to denote where the next "even" element goes
+//* And the other pointer is used to search for elements
 function sortArrayByParity(nums) {
   //* There is nothing to sort
   if (nums.length <= 1) return nums;
 
-  let left = 0; //* Index of next even
-  let right = nums.length - 1; //* Index of next odd
+  let left = 0; //* Marks the index of the next even element
+  let right = 0; //* Travels through the array searching for even elements
 
-  while (left < right) {
-    if ((nums[left] & 1) === 0) {
-      left++; //* Element at left is already even, move on
-    } else if (nums[right] & 1) {
-      right--; //* Element at right is already odd, move on
-    } else {
-      //* Elements are out of place, so swap them
-      [nums[left], nums[right]] = [nums[right], nums[left]];
+  while (right < nums.length) {
+    //* Check if the current element is even
+    if (!(nums[right] & 1)) {
+      swap(nums, left, right);
+      left++;
     }
+
+    right++;
   }
 
   return nums;
 }
 
+function swap(nums, left, right) {
+  const temp = nums[left];
+  nums[left] = nums[right];
+  nums[right] = temp;
+}
+
 console.log(sortArrayByParity([3, 1, 2, 4])); //* [2, 4, 3, 1]
 console.log(sortArrayByParity([0])); //* [0]
-console.log(sortArrayByParity([5, 1, 2, 5, 9, 2, 4, 8, 1]));
+console.log(sortArrayByParity([5, 1, 2, 5, 9, 2, 4, 8, 1])); //* [2, 2, 4, 8, 9, 1, 5, 5, 1]
 
-//* Time: O(n) - We iterate through the entire array once
+//* Time: O(n) - It takes O(n) to iterate through the entire array
 
 //* Space: O(1) - The memory usage remains constant regardless of input size
--- 1.5. Two Pointers II/3. Sort Array by Parity/1. Brute Force O(n) O(n).js
+++ 1.5. Two Pointers II/3. Sort Array by Parity/1. Brute Force O(n) O(n).js
@@ -1,6 +1,7 @@
-//* Separate all of the elements in nums into even and odd arrays
-//* Then, simply spread all of the even elements into a new result array
-//* And lastly spread the remaining odd elements into the result
+//* We are given an int[] and we need to move all of the EVEN integers to the start
+//* Inadvertently, this also has a side effect of moving all of the ODD integers to the end
+//* We can simply create two arrays (odd and even) and group all of the respective elements
+//* Then, we can create a new array using the contents of `even`, followed by the contents of `odd`
 function sortArrayByParity(nums) {
   //* There is nothing to sort
   if (nums.length <= 1) return nums;
@@ -8,20 +9,18 @@ function sortArrayByParity(nums) {
   const even = [];
   const odd = [];
 
-  //* Separate the elements that are even and odd
+  //* Group up the elements based on parity
   for (let i = 0; i < nums.length; i++) {
     nums[i] & 1 ? odd.push(nums[i]) : even.push(nums[i]);
   }
 
-  //* Return an array with all the evens first
   return [...even, ...odd];
 }
 
 console.log(sortArrayByParity([3, 1, 2, 4])); //* [2, 4, 3, 1]
 console.log(sortArrayByParity([0])); //* [0]
-console.log(sortArrayByParity([5, 1, 2, 5, 9, 2, 4, 8, 1]));
+console.log(sortArrayByParity([5, 1, 2, 5, 9, 2, 4, 8, 1])); //* [2, 2, 4, 8, 5, 1, 5, 9, 1]
 
-//* Time: O(n) - It takes O(n) to separate the elements
-//* Then O(n) to create the result array
+//* Time: O(n) - Iterating through the input takes O(n), as does creating the returned array
 
-//* Space: O(n) - The result array is an entirely new array of the same length as nums
+//* Space: O(n) - The total memory used scales with the input
