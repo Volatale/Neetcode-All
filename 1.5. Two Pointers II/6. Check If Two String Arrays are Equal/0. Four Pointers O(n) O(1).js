0f20b9cc62546b3fc14f4682eafdaf08d13863e1
volatale <nathanamarirose@gmail.com>
2025-05-30 00:00:56 +0100

    Update Check If Two String Arrays are Equal

--- 1.5. Two Pointers II/6. Check If Two String Arrays are Equal/0. Four Pointers O(n) O(1).js
+++ 1.5. Two Pointers II/6. Check If Two String Arrays are Equal/0. Four Pointers O(n) O(1).js
@@ -1,52 +1,58 @@
-//* Simply iterate through each character in every word
-//* There is no need to actually build the strings at all
-//* We need two pointers to track the progress through each string array
-//* But we also need two MORE pointers to track the progress WITHIN each word (word[i])
-//*     - If the char pointer is at the end of word, reset the pointer and move to the next word
-//*     - Otherwise, just increment to move onto the next character
-//* Ultimately, the goal is to compare the characters that each pointer points to
-//*     - If they are equal, then everything is fine, check the rest of the characters
-//*     - If they are NOT equal, the strings cannot be equal so return false
+//* We are given two arrays of strings (each of which can be concatenated into another string)
+//* The goal is to compare every character in each inner string to see if the "concatenated" strings are equal
+//* We "could" use the array `join` method to combine the strings, however, we have another option
+//* The inputs are string[], which means we can use a two pointer approach
+//*     - The strings can be broken up into multiple strings
+//* Note that the input arrays are not guaranteed to have the same length, so we can't rely on string comparisons
+//*     - Instead, we need to rely on character comparisons
+//! There is no need to compare the concatenated strings themselves
+//*     - We can instead compare each character in a row
+//* Compare two characters at a time (one in each string)
+//*     - If at any point the comparison is false, then the concatenated strings cannot possibly be equal
+//* To handle the comparisons, we can use two pointers to select the string in a string[]
+//* Then, use two ADDITIONAL pointers to compare each character within that string
 function arrayStringsAreEqual(word1, word2) {
-  let p1 = 0;
-  let p2 = 0; //* Character Pointers
+  //* String pointers
   let w1 = 0;
-  let w2 = 0; //* Word Pointers
+  let w2 = 0;
+
+  //* Character pointers
+  let c1 = 0;
+  let c2 = 0;
 
   while (w1 < word1.length && w2 < word2.length) {
     const currWord1 = word1[w1];
     const currWord2 = word2[w2];
 
-    //* The characters don't match
-    if (currWord1[p1] !== currWord2[p2]) return false;
+    //* The characters don't match, so the strings are not equal
+    if (currWord1[c1] !== currWord2[c2]) return false;
 
-    if (p1 < currWord1.length - 1) {
-      p1++;
+    if (c1 < currWord1.length - 1) {
+      c1++; //* Move to the next character within this word
     } else {
       //* Move onto the next word
-      p1 = 0;
       w1++;
+      c1 = 0;
     }
 
-    if (p2 < currWord2.length - 1) {
-      p2++;
+    if (c2 < currWord2.length - 1) {
+      c2++;
     } else {
-      //* Move onto next word
-      p2 = 0;
       w2++;
+      c2 = 0;
     }
   }
 
-  //* If both pointers made it to the end, the strings were equal
+  //* After all that, if the pointers made it to the end then both strings are equal
   return w1 === word1.length && w2 === word2.length;
 }
 
-console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"])); //* true
-console.log(arrayStringsAreEqual(["a", "cb"], ["ab", "c"])); //* false
-console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])); //* true
-console.log(arrayStringsAreEqual(["sonic"], [""])); //* false
-console.log(arrayStringsAreEqual(["jas"], ["jes"])); //* false
+console.log(arrayStringsAreEqual(["ab", "c"], ["a", "bc"])); //* True
+console.log(arrayStringsAreEqual(["a", "cb"], ["ab", "c"])); //* False
+console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])); //* True
+console.log(arrayStringsAreEqual(["sonic"], [""])); //* False
+console.log(arrayStringsAreEqual(["jas"], ["jes"])); //* False
 
-//* Time: O(n + m) - We have to iterate through every character in every string in both arrays
+//* Time: O(n + m) - Iterating through every character in all the strings scales with the total number of characters in both
 
 //* Space: O(1) - The memory usage remains constant regardless of input size
--- 1.5. Two Pointers II/6. Check If Two String Arrays are Equal/1. Array Join O(n + m) O(n + m).js
+++ 1.5. Two Pointers II/6. Check If Two String Arrays are Equal/1. Array Join O(n + m) O(n + m).js
@@ -1,4 +1,6 @@
-//* Simply convert both arrays into strings and compare them
+//* We are given two arrays of strings (each of which can be concatenated into another string)
+//* The goal is to compare every character in each inner string to see if the "concatenated" strings are equal
+//* Since they are string[], we can simply "join" the arrays using a function and compare at the very end
 function arrayStringsAreEqual(word1, word2) {
   return word1.join("") === word2.join("");
 }
@@ -9,6 +11,6 @@ console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"])); //* true
 console.log(arrayStringsAreEqual(["sonic"], [""])); //* false
 console.log(arrayStringsAreEqual(["jas"], ["jes"])); //* false
 
-//* Time: O(n + m) - We have to iterate through every character in every string in both arrays
+//* Time: O(n + m) - The time taken to join each string scales with the length of each respective input
 
-//* Space: O(n + m) - Two strings of length n and m respectively are created
+//* Space: O(n + m) - The `join()` method returns a new string, so the total memory usage scales with the length of both
