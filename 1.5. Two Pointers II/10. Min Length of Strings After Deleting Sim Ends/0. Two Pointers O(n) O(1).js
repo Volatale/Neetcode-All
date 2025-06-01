421f0b6307de1140b685f907ca133d433db87987
volatale <nathanamarirose@gmail.com>
2025-06-01 14:48:31 +0100

    Update Min Length of Strings After Del Sim Ends

--- 1.5. Two Pointers II/10. Min Length of Strings After Deleting Sim Ends/0. Two Pointers O(n) O(1).js
+++ 1.5. Two Pointers II/10. Min Length of Strings After Deleting Sim Ends/0. Two Pointers O(n) O(1).js
@@ -1,29 +1,32 @@
-//* We need to get a prefix AND a suffix simultaneously
-//* This entails checking TWO characters at the same time
-//* So why not just use a Two pointer Approach?
-//*     - left starts at index 0 and tracks prefixes
-//*     - right starts from s.length - 1 and tracks suffixes
-//* Ultimately, the goal is to ensure both left and right point to the same character
-//* We can match any number of the same character consecutively
-//* So if we had "abbaa"
-//*     - Then left would point to index 1 after the first loop iteration
-//*     - And right would point to index 2 (because we found TWO "a")
+//* We are given a string consisting only of "a", "b" and "c"
+//* The goal is to pick (non-empty) prefixes and suffixes of identical characters from both sides simultaneously
+//* The characters used in the prefixes and suffixes must also not intersect in any order
+//*     - In other words, we can't reuse characters/indices
+//* After all of the conditions are met, we delete both the prefix and suffix
+//* Keep repeating the process until no deletions are possible
+//* Essentially, we need to compare characters from both ends simultaneously
+//*     - A two pointer approach will help immensely
+//! "Deletions" are handled by simply incrementing/decrementing the pointers
+//* Since the goal is to "remove" as many characters as possible, we want to "maximize" the amount of characters deleted
+//* Ultimately, that means we need to find as many duplicate characters as possible
+//* To get the length of the final string, we can simply evaluate (right - left)
 function minimumLength(s) {
-  //* We can't remove any characters
   if (s.length <= 1) return s.length;
 
-  let left = 0; //* For prefix characters
-  let right = s.length - 1; //* For suffix characters
-  let char = ""; //* The current char both pointers must equal
+  //* Two pointers used to find prefix/suffix characters
+  let left = 0;
+  let right = s.length - 1;
+  let char = "";
 
   while (left < right && s[left] === s[right]) {
     char = s[left]; //* Both pointers must point to this character
 
+    //* Eliminate all of the duplicate characters
     while (left < right && s[left] === char) left++;
     while (left < right && s[right] === char) right--;
   }
 
-  //* If s[left] === last deleted char, you deleted the entire string (xyx)
+  //* If s[left] !== char, then we know we didn't eliminate the entire string
   return s[left] === char ? 0 : right - left + 1;
 }
 
@@ -34,4 +37,8 @@ console.log(minimumLength("aabccabba")); //* 3
 console.log(minimumLength("w")); //* 1
 console.log(minimumLength("")); //* 0
 console.log(minimumLength("aa")); //* 0
-// console.log(minimumLength("cac")); //* 1
+console.log(minimumLength("cac")); //* 1
+
+//* Time: O(n) - The time taken scales with the input size
+
+//* Space: O(1) - The memory usage remains constant regardless of input size
