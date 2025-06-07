14fbc57d08793faf71115ce6a0eaa3fd9dcda1bf
volatale <nathanamarirose@gmail.com>
2025-06-07 03:12:18 +0100

    Update Contains Duplicate II

--- 2. Sliding Window/1. Contains Duplicate II/0. Set O(n) O(k).js
+++ 2. Sliding Window/1. Contains Duplicate II/0. Set O(n) O(k).js
@@ -1,18 +1,28 @@
-//* We are essentially keeping a k-sized window worth of elements within the set
-//* The set allows us to have Θ(1) lookup time - the input size is not constrained, so using an array is worse
-//* If there are more than "k" elements within the set at the end of each iteration, we drop the i-kth element
-function containsDuplicateII(nums, k) {
-  const set = new Set(); //* Θ(1) lookup
+//* Given an int[] `nums`, and an int `k`, we need to find a duplicate such that:
+//*     - i !== j
+//*     - nums[i] === nums[j]
+//*     - abs(i - j) <= k
+//* Essentially, there cannot be more than `k` elements between the two indices
+//*     - `j` is guaranteed to be larger than `i`, so we can do something like "arr[n - k] <= k"
+//* Since the elements that are outside of this range cannot be considered, we can "eliminate" them from consideration
+//* Naturally, maps/sets are applicable to problems that involve detecting duplicates
+//* All we have to do is track "current" elements (that are within the valid range) and perform membership tests
+//* Elements that are no longer within the range can be removed from the set
+//! In other words, we maintain a sliding window of elements, and remove the invalid elements
+function containsNearbyDuplicate(nums, k) {
+  const set = new Set();
 
   for (let i = 0; i < nums.length; i++) {
+    //* The array contains a nearby duplicate
     if (set.has(nums[i])) return true;
 
     set.add(nums[i]);
 
-    //* Delete the i-kth element from the set so we can retain O(k) space
+    //* Delete the elements that will fall out of the valid range
     if (set.size > k) set.delete(nums[i - k]);
   }
 
+  //* There are no nearby duplicates
   return false;
 }
 
@@ -20,7 +30,6 @@ console.log(containsDuplicateII([1, 2, 3, 1], 3)); //* True
 console.log(containsDuplicateII([1, 0, 1, 1], 1)); //* True
 console.log(containsDuplicateII([1, 2, 3, 1, 2, 3], 2)); //* False
 
-//* Time: O(n) - The time taken by the loop scales with the size of the input
+//* Time: O(n) - The time taken scales with the input size
 
-//* Space: O(k) - There will be, at most, "k + 1" elements within the set at once
-//* If > k elements exist within the set at once, we delete the i-kth element
+//* Space: O(k) - The memory usage scales with `k` (the set's size never exceeds k)
