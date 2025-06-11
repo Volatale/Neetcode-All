57f267426b2b3542e41ec63c60313ef350533fb5
volatale <nathanamarirose@gmail.com>
2025-06-12 00:05:56 +0100

    Update Find All Anagrams in String

--- 2. Sliding Window/16. Find All Anagrams in String/0. Sliding Window O(n) O(1).js
+++ 2. Sliding Window/16. Find All Anagrams in String/0. Sliding Window O(n) O(1).js
@@ -1,53 +1,62 @@
-//* We are given two strings, and we need to find "p" in "s"
-//* So we know it has to be a substring searching problem
-//* Get the frequency of characters in "p", and do t he same for "s" up to p.length
-//* Then just keep a fixed window size of p.length and check for anagrams along the way
-function findAllAnagrams(s, p) {
-  if (s.length < p.length) return [];
-
+//* Given two strings `s` and `p`, we need to return the start indices of all p's anagrams in `s`
+//* Naturally, anagram-style questions are easily handled by frequency maps
+//* This is fundamentally a substring searching problem
+//* We can use two base 26 arrays ([0] = "a" and [25] = "z")
+//* A sliding window approach allows us to scan windows of characters
+//* Why a sliding window? Because we know that every valid substring will be of p.length
+//* Generate the anagram frequency for `p`
+//* Then, move the sliding window through `s` and check for the valid substrings
+//* We are specifically looking for windows of size `p.length`
+//*     - So there is no need to repeatedly shrink the window using a while loop
+//*     - The moment that we remove the leftmost character, the window will no longer be valid
+function findAnagrams(s, p) {
   const results = [];
 
-  //* Sliding Window
+  //* Stores the exact frequency of characters we need to find in `s`
+  const pFreq = new Array(26).fill(0);
+  const sFreq = new Array(26).fill(0);
+
+  //* Pointers to mark the start/end of the sliding window
   let start = 0;
   let end = 0;
 
-  //* Base 26 Arrays
-  const freq1 = new Array(26).fill(0);
-  const freq2 = new Array(26).fill(0);
-
-  //* Generate the anagram frequency for "p"
+  //* Get the frequency of characters in `p`
   for (let i = 0; i < p.length; i++) {
-    freq1[p[i].charCodeAt(0) - 97]++;
+    pFreq[p[i].charCodeAt(0) - 97]++;
   }
 
   while (end < s.length) {
-    freq2[s[end++].charCodeAt(0) - 97]++;
+    //* Add the character to the window (substring)
+    sFreq[s[end].charCodeAt(0) - 97]++;
 
-    //* If there are at least p.length characters in the window
+    //* If there are at least `p.length` characters in the window
     if (end - start + 1 === p.length) {
-      if (isAnagram(freq1, freq2)) results.push(start);
-      freq2[s[start++].charCodeAt(0) - 97]--;
+      if (isAnagram(sFreq, pFreq)) results.push(start);
+
+      //* Remove the character from the window (substring)
+      sFreq[s[start++].charCodeAt(0) - 97]--;
     }
+
+    end++;
   }
 
   return results;
 }
 
+//* There are only 26 characters in the lowercase alphabet, so this takes O(26) -> O(1)
 function isAnagram(arr1, arr2) {
-  for (let i = 0; i < 26; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
   }
 
   return true;
 }
 
-console.log(findAllAnagrams("cbaebabacd", "abc")); //* [0, 6]
-console.log(findAllAnagrams("abab", "ab")); //* [0, 1, 2]
-console.log(findAllAnagrams("sonic", "sonic")); //* [0]
+console.log(findAnagrams("cbaebabacd", "abc")); //* [0, 6]
+console.log(findAnagrams("abab", "ab")); //* [0, 1, 2]
+console.log(findAnagrams("sonic", "sonic")); //* [0]
 
-//* Time: O(n) - It takes O(p) time to iterate over "p" to get the first window
-//* Then it takes O(n) time (s.length) to iterate over s
-//* Checking for anagrams takes O(26) -> O(1) time, so constant time
+//* Time: O(n + m) - The time takens scales with the lengths of both `s` and `p`
+//* It always takes O(26) -> O(1) to check for anagrams
 
-//* Space: O(26) -> O(1) - The input is limited to lowercase alphabet
-//* The frequency arrays will always use the same amount of space regardless of input size
+//* Space: O(26) -> O(1) - The memory usage remains constant regardless of input size
