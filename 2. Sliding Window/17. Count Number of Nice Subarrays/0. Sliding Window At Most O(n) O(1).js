7f9c8079bae69dd7e05b279d6bdfea91d606b1d9
volatale <nathanamarirose@gmail.com>
2025-06-12 23:59:44 +0100

    Update Count of Nice Subarrays

--- 2. Sliding Window/17. Count Number of Nice Subarrays/0. Sliding Window At Most O(n) O(1).js
+++ 2. Sliding Window/17. Count Number of Nice Subarrays/0. Sliding Window At Most O(n) O(1).js
@@ -1,12 +1,17 @@
-//* The first call gives us the number of subarrays with at MOST "k" odd elements
-//* So we are essentially overestimating how many subarrays we will have
-//* Then we subtract the result of atMost(k-1) which gives us atMost(k-1) odd elements
-//* [2, 2, 2, 1], k = 2 would give us:
-//* atMost(2) = 10
-//* atMost(1) = 6
-//* 10 - 6 = 4
-function countNumberOfNiceSubarrays(nums, k) {
-  //* Exactly(k) = atMost(k) - atMost(k-1)
+//* Given an int[] and an int `k`, a continous subarray is "nice" if there are `k` odd numbers in it
+//*     - In other words, a "nice" subarray contains EXACTLY `k` odd numbers
+//* Since we know exactly how many odd elements we need at any given point, we can use a sliding window approach
+//* The only issue is that is that a pure sliding window approach may lead to us missing some possible subarrays/
+//* So, we can use the "atMost `k`" variation of sliding window
+//*     - Exactly(k) = atMost(k) - atMost(k - 1)
+//*     - The former call gives every possible subarray, and the latter call removes the extra (unneeded) subarrays
+//! If we ever get to a point where "oddCount" > k, then we should immediately shrink the window until the invariant is upheld
+//*     - Why? Because there needs to be EXACTLY `k` odd integers, not greater than or equal to `k`
+//*     - Extending the subarray beyond this point would never lead to a possible solution
+//* The numbe of odds will never decrease, it only ever stays the same or increases
+//* To check for an odd integer, we can simply check the LSB of the number
+//*     - if (n & 1) === 1, then it is an odd number, else it is even
+function numberOfSubarrays(nums, k) {
   return atMost(nums, k) - atMost(nums, k - 1);
 }
 
@@ -14,21 +19,21 @@ function atMost(nums, k) {
   let odds = 0;
   let subarrays = 0;
 
-  //* Sliding Window
+  //* Pointers that mark the start/end of the sliding window
   let start = 0;
   let end = 0;
 
   while (end < nums.length) {
-    //* Check if the LSB is set, if so, it is an odd number
-    if (nums[end] & 1) odds++;
+    //* If the LSB is set, then it is an odd number
+    if (nums[end] & 1) {
+      odds++;
+    }
 
+    //* Ensure the invariant is upheld (window never contains > `k` odd elements)
     while (odds > k) {
-      //* Remove the leftmost element
-      //* If it is an odd, it'll be 1, otherwise 0
       odds -= nums[start++] & 1;
     }
 
-    //* Every valid subarray within this range
     subarrays += end - start + 1;
     end++;
   }
@@ -36,15 +41,14 @@ function atMost(nums, k) {
   return subarrays;
 }
 
-console.log(countNumberOfNiceSubarrays([2, 2, 2, 1], 1)); //* 4
-console.log(countNumberOfNiceSubarrays([1, 1, 2, 1, 1], 3)); //* 2
-console.log(countNumberOfNiceSubarrays([2, 4, 6], 1)); //* 0
-console.log(countNumberOfNiceSubarrays([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2)); //* 16
-console.log(countNumberOfNiceSubarrays([1, 1, 1, 1, 1], 4)); //* 2
-console.log(countNumberOfNiceSubarrays([1, 1, 2, 3], 2)); //* 3
-console.log(countNumberOfNiceSubarrays([1], 1)); //* 1
+console.log(numberOfSubarrays([2, 2, 2, 1], 1)); //* 4
+console.log(numberOfSubarrays([1, 1, 2, 1, 1], 3)); //* 2
+console.log(numberOfSubarrays([2, 4, 6], 1)); //* 0
+console.log(numberOfSubarrays([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2)); //* 16
+console.log(numberOfSubarrays([1, 1, 1, 1, 1], 3)); //* 3
+console.log(numberOfSubarrays([1], 1)); //* 1
+console.log(numberOfSubarrays([1, 2, 2, 1, 2, 1, 1], 2)); //* 7
 
-//* Time: O(n) - It takes O(n) to complete "atMost"
-//* We call atMost twice, so O(2n) simplifies to O(n)
+//* Time: O(n) - Each call to atMost() takes O(n), so we have a O(2n) time complexity, but this simplifies to O(1)
 
-//* Space: O(1) - We only use constant space variables, so the space usage does not scale with input size
+//* Space: O(1) - The memory usage remains constant regardless of the input size
--- 2. Sliding Window/17. Count Number of Nice Subarrays/1. Nested Loops O(n^2) O(1).js
+++ 2. Sliding Window/17. Count Number of Nice Subarrays/1. Nested Loops O(n^2) O(1).js
@@ -1,29 +1,42 @@
-//* Use a nested for loop and check every subarray
-//* Count the number of odd elements
-//* n & 1 checks if the LSB is set
-//* 011 & 001 = 001
-function numberOfNiceSubarrays(nums, k) {
-  let subarrays = 0;
+//* Given an int[] and an int `k`, a continous subarray is "nice" if there are `k` odd numbers in it
+//*     - In other words, a "nice" subarray contains EXACTLY `k` odd numbers
+//* If we take a brute force approach, we could simply compute every possible subarray
+//! If we ever get to a point where "oddCount" > k, then we should immediately break out of the inner loop
+//*     - Why? Because there needs to be EXACTLY `k` odd integers, not greater than or equal to `k`
+//*     - Extending the subarray beyond this point would never lead to a possible solution
+//* The numbe of odds will never decrease, it only ever stays the same or increases
+//* To check for an odd integer, we can simply check the LSB of the number
+//*     - if (n & 1) === 1, then it is an odd number, else it is even
+function numberOfSubarrays(nums, k) {
+  let niceSubarrays = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    let odd = 0; //* Count the number of odd elements
+    let odds = 0;
 
     for (let j = i; j < nums.length; j++) {
-      if (nums[j] & 1) odd++; //* Checks if the element is odd
-      if (odd === k) subarrays++;
+      //* Check if nums[j] is odd
+      if (nums[j] & 1) {
+        odds++;
+      }
+
+      //* Add occurrences of nice subarrays
+      if (odds === k) {
+        niceSubarrays++;
+      } else if (odds > k) break;
     }
   }
 
-  return subarrays;
+  return niceSubarrays;
 }
 
-console.log(numberOfNiceSubarrays([1, 1, 2, 1, 1], 3)); //* 2
-console.log(numberOfNiceSubarrays([2, 4, 6], 1)); //* 0
-console.log(numberOfNiceSubarrays([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2)); //* 16
-console.log(numberOfNiceSubarrays([1, 1, 1, 1, 1], 3)); //* 3
-console.log(numberOfNiceSubarrays([1], 1)); //* 1
-console.log(numberOfNiceSubarrays([1, 2, 2, 1, 2, 1, 1], 2));
+console.log(numberOfSubarrays([1, 1, 2, 1, 1], 3)); //* 2
+console.log(numberOfSubarrays([2, 4, 6], 1)); //* 0
+console.log(numberOfSubarrays([2, 2, 2, 1, 2, 2, 1, 2, 2, 2], 2)); //* 16
+console.log(numberOfSubarrays([1, 1, 1, 1, 1], 3)); //* 3
+console.log(numberOfSubarrays([1], 1)); //* 1
+console.log(numberOfSubarrays([1, 2, 2, 1, 2, 1, 1], 2)); //* 7
 
-//* Time: O(n^2) - We have a nested for loop, both of which scale with "n"
+//* Time: O(n^2) - The time taken scales quadratically with the input size
+//* We have two nested for loops, both of which scale with the input size
 
-//* Space: O(1) - We don't use any space that scales with input size
+//* Space: O(1) - The memory usage remains constant regardless of the input size
