a985d43113e9ff3d9887c57c170637a9d22b048e
volatale <nathanamarirose@gmail.com>
2025-06-15 00:02:14 +0100

    Update Permutation in String

--- 2. Sliding Window/5. Permutation in String/2. Sliding Window O(n) O(1).js
+++ 2. Sliding Window/5. Permutation in String/2. Sliding Window O(n) O(1).js
@@ -1,14 +1,18 @@
-//* The question is essentially asking for an Anagram of s1 that exists in s2
-//* A "permutation" can be considered a different order of something
-//* Thus, a permutation and an anagram are effectively the same in this context
-function permutationInString(s1, s2) {
-  if (s2.length < s1.length) return false; //* A permutation is impossible
+//* Given two strings, we need to check if s2 contains a PERMUTATION of s1
+//* A permutation is essentially just some ordering of elements (in this case, characters)
+//* To check for permutations in strings, we can simply check for the frequency
+//!     - A permutation in a string is analogous to an anagram
+//* In a brute force manner, we can get the frequency of characters in s1
+//* Then, for every substring in s2, we can check if the frequencies match
+//*     - If they do, then we now that s2 contains a permutation of s1
+function checkInclusion(s1, s2) {
+  //* There is no possibility of s2 containing a permutation of s1
+  if (s2.length < s1.length) return false;
 
-  //* Sliding Window
   let start = 0;
   let end = 0;
 
-  //* Base 26 Arrays ("a" = 97. "a" - "a" = 0)
+  //* Base 26 arrays ("a" - "a" === 97 - 97 === 0)
   const freq1 = new Array(26).fill(0);
   const freq2 = new Array(26).fill(0);
 
@@ -19,20 +23,22 @@ function permutationInString(s1, s2) {
     end++;
   }
 
+  //* Immediately check if the frequencies are the same
   if (isAnagram(freq1, freq2)) return true;
 
   while (end < s2.length) {
     freq2[s2[end++].charCodeAt(0) - 97]++;
 
-    //* Remove leftmost char from the window
+    //* Remove leftmost char from window (it is leaving window)
     freq2[s2[start++].charCodeAt(0) - 97]--;
 
+    //* Check if the frequencies are the same
     if (isAnagram(freq1, freq2)) return true;
   }
+
   return false;
 }
 
-//* O(26)
 function isAnagram(arr1, arr2) {
   for (let i = 0; i < 26; i++) {
     if (arr1[i] !== arr2[i]) return false;
@@ -41,14 +47,13 @@ function isAnagram(arr1, arr2) {
   return true;
 }
 
-console.log(permutationInString("ab", "bloba")); //* true
-console.log(permutationInString("icson", "sonic")); //* true
-console.log(permutationInString("zw", "wz")); //* true
-console.log(permutationInString("w", "z")); //* false
-console.log(permutationInString("law", "lae")); //* false
+console.log(checkInclusion("ab", "bloba")); //* True
+console.log(checkInclusion("icson", "sonic")); //* True
+console.log(checkInclusion("zw", "wz")); //* True
+console.log(checkInclusion("w", "z")); //* False
+console.log(checkInclusion("law", "lae")); //* False
 
-//* Time: O(n) - We have to process each character in both strings
-//* We also have an O(26) call in isAnagram, but this always takes the same amount of time
+//* Time: O(n * 26) - It takes O(26) to check for an anagram, and it takes O(n) to iterate through s1
 
-//* Space: O(26) -> O(1) - We have two frequency arrays that always have 26 indices
-//* They do not scale in size with either input, so the function uses constant space
+//* Space: O(1) - The memory usage remains constant regardless of input size (of either string)
+//* The frequency arrays are always 26 length
