dce09615b05ba84beaf7c00d85dd0b4fb4cf94b0
volatale <nathanamarirose@gmail.com>
2025-06-16 00:11:24 +0100

    Update Frequency of Most Frequent Element

--- 2. Sliding Window/6. Frequency Of Most Frequent Element/0. Sort Sliding Window O(n log n) O(n).js
+++ 2. Sliding Window/6. Frequency Of Most Frequent Element/0. Sort Sliding Window O(n log n) O(n).js
@@ -1,45 +1,50 @@
-//* Sort the values
-//* K is "decreased" less if values are closer to each other
-//* [1, 1, 3], k = 2, it is easier to turn both 1s into 2s, than into 3s
-
-//* windowSize * nums[end] is the sum of the window WITH the increments
-//* sum + k is the maxValue + the extra increments
-//* [1, 1, 1, 2], end = 3 would mean we are turning all of the 1s into 2s
-//* Which would give us "8", but if k = 2...
-//* 5 + 2 = 7
-//* 8 > 7 so window is invalid; decrease the window size until it is valid
+//* We can choose any index and increment the element that exists there by 1
+//* The goal is to return the maximum possible frequency of an element after performing AT MOST k operations
+//! Immediately, we can observe that the elements will never decrease
+//*     - They either stay the same, or they increase (monotonically non-increasing)
+//! Another observation we can make is that the order of the elements does not matter
+//* Ideally, we want elements that are similar in value to be close together
+//* This allows us to have a better chance of minimizing the amount of operations done
+//* This would give us the highest chance of maximizing the frequency of any element
+//* Then, we can apply a sliding window approach and expand/shrink the window as needed
+//* Track the sum of elements within the window
+//* To check if a window is valid, multiply the window size by nums[end], then subtract the sum from it
+//* If the result is greater than K, then we need to decrease the window size
+//*     - Why? Because we need to do more "increment operations" than is possible (k is too small)
 function maxFrequency(nums, k) {
+  //* Sort the array to ensure monotonicity
   nums.sort((a, b) => a - b);
 
-  //* Sliding Window
+  let sum = 0;
+  let maxFreq = 0;
+
+  //* Pointers for the sliding window start/end
   let start = 0;
   let end = 0;
 
-  let maxLength = 0;
-  let sum = 0;
-
   while (end < nums.length) {
+    //* Add the current value to the window
     sum += nums[end];
 
-    //* windowSize * nums[end] is the sum of the window WITH the increments
-    //* sum + k is the maxValue + the extra increments
-    while ((end - start + 1) * nums[end] > sum + k) {
+    //* window size * nums[end] = current cost to turn entire window into nums[end]
+    //* Subtract from that, the sum (because we don't need to do every operation)
+    while ((end - start + 1) * nums[end] - sum > k) {
       sum -= nums[start++];
     }
 
-    maxLength = Math.max(maxLength, end - start + 1);
+    //* Find a new max frequency
+    maxFreq = Math.max(maxFreq, end - start + 1);
     end++;
   }
 
-  return maxLength;
+  return maxFreq;
 }
 
 console.log(maxFrequency([1, 2, 4], 5)); //* 3
 console.log(maxFrequency([1, 4, 8, 13], 5)); //* 2
 console.log(maxFrequency([3, 9, 6], 2)); //* 1
 
-//* Time: O(n log n) - We have to sort the array to ensure that closer values can be transformed easily
-//* Then we have an O(n) while loop, but that is dominated by the n log n
+//* Time: O(sort) - The time taken scales with the time needed to sort the array
+//* Otherwise, the actual algorithm takes O(n)
 
-//* Space: O(n) - If we are using merge sort, then we are using O(n) space
-//* If it is quick sort, then we are using O(log n) space, and O(1) for heap
+//* Space: O(sort) - The memory usage scales with the sorting algorithm used
