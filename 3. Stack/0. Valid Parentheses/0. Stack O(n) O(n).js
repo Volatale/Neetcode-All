1c690520587af252786b9589c10165f57e8f400b
volatale <nathanamarirose@gmail.com>
2025-06-20 03:50:28 +0100

    Update Valid Parentheses

--- 3. Stack/0. Valid Parentheses/0. Stack O(n) O(n).js
+++ 3. Stack/0. Valid Parentheses/0. Stack O(n) O(n).js
@@ -1,16 +1,23 @@
-//* We need to ensure the correct order, this entails the use of a stack
-//* On encountering an "opener", we push the closer counterpart to the stack
-//* Since a stack is LIFO, the most recent opener will be closed first
-//* If you encounter a closer, make sure the current element is the top of the stack
-//* Else if the top element is not equal to the current element, return false
-//* The edge case is a situation where the stack is not empty
-//* ["(", "("], the stack is [")", ")"] and the default clause never activates
-//* So we should return whether stack.length === 0
-function validParentheses(str) {
+//* Given a string containing {, }, (, ), [, and ], we need to determine if the parentheses are valid
+//* An input string can only be considered value if all of the following constraints are met:
+//*     - Open brackets must be closed by the same type of bracket
+//*     - Open brackets must be closed on the correct order
+//*     - Evrery close brack has a corresponding open bracket of the same type
+//* So we have both an ordering constraint, and a "type" (shape) constraint
+//! The MOST RECENT bracket is the one that much be closed first
+//* This indicates that we need to process brackets in a LIFO order
+//*     - Doing so would also allow us to validate the shapes of the expected parentheses
+//* So, we can use a stack to handle both the ordering and processing
+//*     - A stack would also give us fast access to the top element (which is the only one we care about)
+//* The stack will contain elements that we "need", so if we find a "(", push a ")" onto the stack
+//*     - Therefore, if we don't find a corresponding ")" at some point, the string is invalid
+//! Also note that we can never have a greater amount of closers than openers (of any type)
+function isValid(s) {
+  //* Used to handle the LIFO ordering
   const stack = [];
 
-  for (let i = 0; i < str.length; i++) {
-    switch (str[i]) {
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
       case "(":
         stack.push(")");
         break;
@@ -21,22 +28,20 @@ function validParentheses(str) {
         stack.push("}");
         break;
       default:
-        if (str[i] !== stack.pop()) return false;
+        //* This is not the character we needed
+        if (s[i] !== stack.pop()) return false;
     }
   }
 
   return stack.length === 0;
 }
 
-console.log(validParentheses("()")); //* True
-console.log(validParentheses("()[]{}")); //* True
-console.log(validParentheses("(]")); //* False
-console.log(validParentheses("{}[}")); //* False
-console.log(validParentheses("[]{}")); //* True
+console.log(isValid("()")); //* True
+console.log(isValid("()[]{}")); //* True
+console.log(isValid("(]")); //* False
+console.log(isValid("{}[}")); //* False
+console.log(isValid("[]{}")); //* True
 
-//* Time: O(n) - The time taken scales with "n"
-//* At worst, we add every element to the stack
-//* Then that means there are 2n operations in the absolute worst case
+//* Time: O(n) - The time taken scales with the input size; we have to process every character
 
-//* Space: O(n) - In the worst case, we add every element to the stack
-//* Such as an input like: ["(", "(", "("]. The stack looks like: [")",")", ")"]
+//* Space: O(n) - The stack's size proportionally to the tune of O(n / 2) in the worst case, which simplifies to O(n)
