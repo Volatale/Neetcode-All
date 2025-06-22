15eb3c8c61bfa54230f4ec13aa546aed65ae096b
volatale <nathanamarirose@gmail.com>
2025-06-22 14:44:47 +0100

    Update Simplify Path

--- 3. Stack/12. Simplify Path/0. Stack O(n) O(n).js
+++ 3. Stack/12. Simplify Path/0. Stack O(n) O(n).js
@@ -1,27 +1,37 @@
-//* Use a stack to track our canonical path
-//* A canonical path is the path you ACTUALLY take
-//* So "/home/documents/.." results in "/home"
-//* Split the input via "/" so its easier to work with
-//* If you encounter a ".." pop the last thing in the stack
-//* Ignore ".", ".." and ""
-//* Push everything else to the stack
+//* We are given an absolute path for a Unix-style file system
+//* The goal is to transform it to its simplified canonical path
+//* Rules:
+//*     - A single "." represents the current directory
+//*     - A double ".." represents the previous/parent directory
+//*     - "//", "///", "////" etc are all treated as a single "/"
+//*     - Any sequence of periods that is NOT "." or ".." is treated as the NAME of the file/directory
+//* Path Rules:
+//*     - The path must start with "/"
+//*     - Directories within the path are separated by one "/"
+//*     - Path must not END with "/" unless it is the root directory
+//*     - Path must not have any single / double periods used to denote current or parent directories
+//*         - The canonical path should have already dealt with all of these occurrences
+//! Since we can only go up or down directories, we are processing everything in LIFO order
+//* LIFO processing means we should use a stack data structure
+//*     - If we encounter a "..", we should pop the stack - we go back up one dir
+//* To simplify the process, we can "split" the array via "/"
+//* Then, at the very end, we should "join" the array via "/"
+//* The only symbols that need to be handled are "." and ".."
+//*     - Everything else gets pushed to the stack (becomes part of the canonical path)
 function simplifyPath(path) {
-  //* The stack tracks our canonical path
-  const stack = [];
+  const string = path.split("/"); //* Get all of the individual symbols
+  const canonical = []; //* Stores the canonical path
 
-  //* Remove the "/", we'll add them back later
-  const p = path.split("/");
-
-  for (let i = 0; i < p.length; i++) {
-    if (stack.length > 0 && p[i] === "..") {
-      stack.pop(); //* We aren't ACTUALLY visiting the previous directory, so remove it
-    } else if (p[i] !== "." && p[i] !== "" && p[i] !== "..") {
-      stack.push(p[i]); //* Its a word, so just push it
+  for (let symbol of string) {
+    if (symbol === ".." && canonical.length > 0) {
+      canonical.pop();
+    } else if (symbol !== "." && symbol !== "" && symbol !== "..") {
+      canonical.push(symbol);
     }
   }
 
-  //* Canonical path STARTS with a "/", AND add back the "/" we lost earlier
-  return "/" + stack.join("/");
+  //* Add the prefix "/" and separate all of the directories
+  return "/" + canonical.join("/");
 }
 
 console.log(simplifyPath("/home/"));
@@ -29,11 +39,8 @@ console.log(simplifyPath("/home//foo/"));
 console.log(simplifyPath("/home/user/Documents/../Pictures"));
 console.log(simplifyPath("/../"));
 console.log(simplifyPath("/.../a/../b/c/../d/./"));
+console.log(simplifyPath("/../"));
 
-//* Time: O(n) - It takes O(n) time to generate the the split array
-//* Then, we do another O(n) iteration through the split array
-//* Lastly, it takes O(n) in the worst case to join the "stack"
+//* Time: O(n) - The time taken scales with the input size
 
-//* Space: O(n) - The split array can potentially scale linearly with the input
-//* The rebuilt string may have an idential length to the input
-//* Either way, the space usage scales with the input size
+//* Space: O(n) - In the worst case, the stac's size is equivalent to the input size
