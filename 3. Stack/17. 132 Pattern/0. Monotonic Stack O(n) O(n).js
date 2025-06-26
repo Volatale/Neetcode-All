9316c0e003d51ad5535d2930d1e4687130f469aa
volatale <nathanamarirose@gmail.com>
2025-06-27 00:32:27 +0100

    Update 132 Pattern

--- 3. Stack/17. 132 Pattern/0. Monotonic Stack O(n) O(n).js
+++ 3. Stack/17. 132 Pattern/0. Monotonic Stack O(n) O(n).js
@@ -1,27 +1,28 @@
-//* Use a monotonically decreasing stack
-//* Iterate in reverse because we want to find the next greater element of "2" in "132"
-//* Lets say you have [1, 6, 5, 4, 3, 2, 1]
-//* Every time you pop when you find the NGE, second increases in value
-//* This means that the range of values we can use to test for "1" in "132" increases
-//* If second = 5, and you popped a 10, then now second is 10
-//* Which means any value AFTER the "3" in "132" < 10 can be used as our "1" in "132"
-//* At the start of each iteration, check if you have a value < second
-//* Any value that fits that criteria ALSO fits the "1" in "132" criteria, so return true
-//* This is why second starts as -Infinity, because you could get a false positive otherwise
+//* We are given an array of integers and we need to find 3 numbers that fit the "132 pattern"
+//*     - The "132 pattern" is a SUBSEQUENCE of three integers where i < j < k (the indices) AND
+//*     - nums[i] < nums[k] < nums[j]
+//* So in other words, three distinct indices must be used, and we need to find a smaller number than nums[k]
+//* Mathematically speaking, we want to minimize nums[i] if it is at all possible
+//! We can use a monotonic stack and aim to find the Next Greater Element at each possible index
+//* By doing so, we effectively minimize the value of nums[i]
+//* The goal is to minimize nums[i], but we also want to maximize nums[k]
+//* By minimizing nums[i], the value of nums[k] can be smaller
+//* And by maximizing nums[k], we have a higher chance of finding a value < nums[k]
+//* By iterating backwards, we can find the Next Greater Element of the "2" in "132"
+//* Essentially, we find the "2" first, then we attempt to maximize the "3"
+//*     - By maximizing the 3, we have the highest chance of finding the "1"
+//* Once we find the NGE of "2", we know for sure that there is a number smaller than nums[j]
+//* Thus, all we have do is keep maximizing nums[k] and find a nums[i] < nums[k]
 function find132Pattern(nums) {
-  if (nums.length < 3) return false;
-
-  //* Monotonically Decreasing Stack
+  //* Monotonically decreasing stack to find NSE (used to find nums[j])
   const stack = [];
 
-  //* When we pop, we want to find an element that is LESS than this (this is "2" in "132")
   let second = -Infinity;
 
-  //* Loop backward because we want to maximize the chance of finding the "1" later
   for (let i = nums.length - 1; i >= 0; i--) {
-    if (nums[i] < second) return true; //* "1" in "132"
+    if (nums[i] < second) return true; //* Found nums[j]
 
-    //* Find the next greater element
+    //* Find the NGE of "2" (maximizing "3" gives us a better chance of finding "1")
     while (stack.length > 0 && nums[i] > stack[stack.length - 1]) {
       second = stack.pop();
     }
@@ -38,9 +39,6 @@ console.log(find132Pattern([-1, 3, 2, 0])); //* True
 console.log(find132Pattern([1, 4, 5])); //* False
 console.log(find132Pattern([1, 3, 2])); //* True
 
-//* Time: O(n) - At worst, we push and pop each element twice each
-//* So that would be O(2n), but we drop constants in Big O Notation -> O(n)
+//* Time: O(n) - The time taken scales with the number of elements in the input array
 
-//* Space: O(n) - In the worst case, we have an array like [1, 4, 5]
-//* None of these elements trigger the while loop, thus none are popped
-//* The stack contains "n" elements, which is proportional to the input size
+//* Space: O(n) - In the worst case, there is no "132" pattern, so the stack holds every element (n)
