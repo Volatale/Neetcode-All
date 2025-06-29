54e014f8307424d932e1eca34115c0f72bc0331f
volatale <nathanamarirose@gmail.com>
2025-06-30 00:44:05 +0100

    Update Min Stack

--- 3. Stack/3. Min Stack/0. Min Stack.js
+++ 3. Stack/3. Min Stack/0. Min Stack.js
@@ -1,97 +1,53 @@
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-class MyStack {
-  constructor() {
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
-  }
-
-  empty() {
-    return this.length === 0;
-  }
-
-  peek() {
-    return this.top.val;
-  }
-
-  push(val) {
-    const newNode = new ListNode(val);
-
-    if (this.length === 0) {
-      this.top = newNode;
-      this.bottom = newNode;
-    } else {
-      newNode.next = this.top;
-      this.top = newNode;
-    }
-
-    this.length++;
-    return this.length;
-  }
-
-  pop() {
-    if (this.length === 0) return;
-
-    const popped = this.top;
-
-    if (this.length === 1) {
-      this.top = null;
-      this.bottom = null;
-    } else {
-      this.top = this.top.next;
-    }
-
-    this.length--;
-    return popped.val;
-  }
-}
-
+//* We need to design a stack that supports the following operations:
+//*     - Push
+//*     - Pop (don't return the element)
+//*     - Top (peek)
+//*     - getMin (gets the minimum element in the stack)
+//* So ultimately, we need a way to retrieve the TOP element and also the MINIMUM element
+//* Naturally, this involves tracking whatever the minimum currently is
+//! We could use a Min Heap (Priority Queue) to track the minimum element, but this is inefficient
+//*     - One of the constraints is that each operation must have a time complexity of O(1)
+//* Ideally, we don't use a separate data structure just to handle the tracking of the minimum
+//* We can track the minimum within the stack itself
+//* Whenever we find a value <= current min, push it to the stack (as an extra element)
+//* Then, push whatever the new value is supposed to be
+//* That way, if the popped element happens to be the current minimum
+//*     - We can set the "new" minimum to the NEXT element
+//*     - In other words, we pop twice
+//! Essentially, whenever we find a new minimum, we push the old minimum AND the new minimum
+//* And when the new minimum is removed, this allows us to easily determine what the previous minimum was (before the new one)
 class MinStack {
+  #min;
+  #stack;
+
   constructor() {
-    this.min = Infinity;
-    this.stack = new MyStack();
+    this.#min = Infinity;
+    this.#stack = [];
   }
 
-  //* If val <= min, push the CURRENT min onto the stack
-  //* This ensures that if we pop the min, we have an immediate reference to the PREVIOUS minimum
-  //* [Inf, 5], min = 5, then push 2, [Inf, 5, 5, 2]. Min = 2
-  //* Popping 2 puts 5 on top of the stack, then we pop that too
   push(val) {
-    if (val <= this.min) {
-      this.stack.push(this.min); //* Add the PREVIOUS min to the stack
-      this.min = val;
+    if (val <= this.#min) {
+      this.#stack.push(this.#min); //* Push the current minimum
+      this.#min = val; //* Now we have a new minimum
     }
 
-    this.stack.push(val);
+    this.#stack.push(val);
   }
 
-  //* If you just found a new minimu, the previous minimum is always right underneath the top
-  //* So we have to pop 2
   pop() {
-    if (this.stack.pop() === this.min) this.min = this.stack.pop();
+    const popped = this.#stack.pop();
+
+    //* The current minimum was removed, so now we need the previous min
+    if (popped === this.#min) {
+      this.#min = this.#stack.pop();
+    }
   }
 
   top() {
-    return this.stack.peek();
+    return this.#stack[this.#stack.length - 1];
   }
 
   getMin() {
-    return this.min;
+    return this.#min;
   }
 }
-
-const minStack = new MinStack();
-
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-console.log(minStack.getMin()); //* -3
-minStack.pop();
-console.log(minStack.top()); //* 0
-console.log(minStack.getMin()); //* -2
