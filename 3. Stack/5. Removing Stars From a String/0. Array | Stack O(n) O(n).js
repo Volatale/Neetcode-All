a2efaaf17de328b78ca4f33551c935ef5584ba81
volatale <nathanamarirose@gmail.com>
2025-07-07 00:07:59 +0100

    Update Removign Stars From a String

--- 3. Stack/5. Removing Stars From a String/0. Array | Stack O(n) O(n).js
+++ 3. Stack/5. Removing Stars From a String/0. Array | Stack O(n) O(n).js
@@ -1,26 +1,25 @@
-//* Push non-stars to the array
-//* Pop when we find a star
-function removingStarsFromAString(s) {
-  //* String Builder
-  const stack = [];
+//* We are given a string of stars (*) and characters
+//* In one operation, we can choose a star, then remove the closest non-star to its left
+//* Ultimately, we need to return the string after all of the stars have been removed
+//*     - So we need to track the string as we go and perform the "deletions" on the spot
+//* The easiest thing to do is simply remove the most recent character whenever we encounter a *
+//! We can use a stack to get the "most recent" character since they support LIFO operations
+function removeStars(s) {
+  //* Use an array as a string builder
+  const string = [];
 
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "*") {
-      stack.pop();
-    } else {
-      stack.push(s[i]);
-    }
+  for (let char of s) {
+    char !== "*" ? string.push(char) : string.pop();
   }
 
-  return stack.join("");
+  return string.join("");
 }
 
-console.log(removingStarsFromAString("leet**cod*e")); //* "lecoe"
-console.log(removingStarsFromAString("leet**")); //* "le"
-console.log(removingStarsFromAString("le*ll*w")); //* "llw"
-console.log(removingStarsFromAString("erase*****")); //* ""
+console.log(removeStars("leet**cod*e")); //* "lecoe"
+console.log(removeStars("leet**")); //* "le"
+console.log(removeStars("le*ll*w")); //* "llw"
+console.log(removeStars("erase*****")); //* ""
 
-//* Time: O(n) - We iterate over the entire string, so the time taken scales with "n"
+//* Time: O(n) - We iterate over every character in the input string
 
-//* Space: O(n) - In the worst case, every character is a non-star
-//* So for example: ["s", "o", "n", "i", "c"] returns "sonic"
+//* Space: O(n) - The "stack" size grows linearly with the size of the input
