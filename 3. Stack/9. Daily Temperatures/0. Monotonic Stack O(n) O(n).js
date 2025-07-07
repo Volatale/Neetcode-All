fb5596fa3d55b767b8c54b222dc8423b63df6d15
volatale <nathanamarirose@gmail.com>
2025-07-08 00:00:27 +0100

    Update Daily Temperatures

--- 3. Stack/9. Daily Temperatures/0. Monotonic Stack O(n) O(n).js
+++ 3. Stack/9. Daily Temperatures/0. Monotonic Stack O(n) O(n).js
@@ -1,37 +1,37 @@
-//* Use a monotonic stack to find the next greater element
-//* When you do, you want to pop the top of the stack
-//* Within the stack, we store INDICES, not the values themselves
-//* This allows us to replicate the "j - i" from the brute force solution
-//* Grab the "index" from the stack, and use that to compare with the current
-//* temp[stack[stack.length - 1]] = 73, compared with 74. So we pop the 73 since it was the NEXT greater element
+//* We are given an array `temperatures`, which represents the daily temperature
+//* Return an array `answer` such that answer[i] is the no. of days after "i" for a warmer temp
+//*     - If there is no future day for which this is possible, answer[i] === 0
+//* In other words, we need the next greater value for each index (day)
+//* To find the next greater value, we can simply use a monotonic stack
+//* The stack stores monotonically non-increasaing values
+//* Since all of the values are NOT greater than the current, we still need a larger value
+//* Therefore, the NEXT greater element is the NGE for ALL of the elements in the stack
+//* So we can severely cut down on the time complexity
 function dailyTemperatures(temperatures) {
-  const result = new Array(temperatures.length).fill(0);
-  const stack = []; //* Find Next Greater Element
+  //* Monotonically decreasing stack - stores indices of days we need NGE of
+  const stack = [];
+  const answer = new Array(temperatures.length).fill(0);
 
   for (let i = 0; i < temperatures.length; i++) {
     while (
-      stack.length > 0 && //* Check if you ever have an element to pop
-      temperatures[i] > temperatures[stack[stack.length - 1]] //* If the element we WANT to add > top of the stack
+      //* Found next greater element of all elements in the stack
+      stack.length > 0 &&
+      temperatures[stack[stack.length - 1]] < temperatures[i]
     ) {
-      const top = stack.pop();
-      result[top] = i - top; //* Number of days we have to wait
+      const index = stack.pop();
+      answer[index] = i - index;
     }
 
-    //* Pushing the value itself won't let us get the difference in days
-    //* So instead we push "i", then we can still index into temperatures and compare values
     stack.push(i);
   }
 
-  return result;
+  return answer;
 }
 
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //* [1, 1, 4, 2, 1, 1, 0, 0]
 console.log(dailyTemperatures([30, 40, 50, 60])); //* [1, 1, 1, 0]
 console.log(dailyTemperatures([30, 60, 90])); //* [1, 1, 0]
 
-//* Time: O(n) - We have an outer loop, and then an inner while loop
-//* The inner while loop will only activate at most "n" times
+//* Time: O(n) - Each element is processed at most twice, so the time complexity is linear
 
-//* Space; O(n) - The result array scales proportionally with the size of the input
-//* If the input is [10, 20, 30], that is length 3
-//* The result will be: [1, 1, 0], also length 3
+//* Space: O(n) - The stack grows proportionally in size to the input size (in the worst case)
--- 3. Stack/9. Daily Temperatures/1. Nested Loops O(n^2) O(n).js
+++ 3. Stack/9. Daily Temperatures/1. Nested Loops O(n^2) O(n).js
@@ -1,30 +1,27 @@
-//* Find the next greater element to the right
-//* "i" represents the current day
-//* "j" represents how many days we have to wait until the next greater temperature
-//* So j - i gives us the number of days until temperatures[i] has to wait for a better temp
+//* We are given an array `temperatures`, which represents the daily temperature
+//* Return an array `answer` such that answer[i] is the no. of days after "i" for a warmer temp
+//*     - If there is no future day for which this is possible, answer[i] === 0
+//* In other words, we need the next greater value for each index (day)
+//* In a brute force manner, we can simply use a nested for loop
 function dailyTemperatures(temperatures) {
-  const result = new Array(temperatures.length).fill(0);
+  const answer = new Array(temperatures.length).fill(0);
 
   for (let i = 0; i <= temperatures.length - 1; i++) {
     for (let j = i + 1; j < temperatures.length; j++) {
       if (temperatures[j] > temperatures[i]) {
-        result[i] = j - i;
+        answer[i] = j - i;
         break;
       }
     }
   }
 
-  return result;
+  return answer;
 }
 
 console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); //* [1, 1, 4, 2, 1, 1, 0, 0]
 console.log(dailyTemperatures([30, 40, 50, 60])); //* [1, 1, 1, 0]
 console.log(dailyTemperatures([30, 60, 90])); //* [1, 1, 0]
 
-//* Time: O(n^2) - We have a nested loop, both of which depend on the input size
-//* For each outer loop, there are "n" inner loops
-//* So the time complexity ends up being O(n * (n + 1) / 2), but that simplifies to O(n^2)
+//* Time: O(n^2) - We have a nested for loop, both of which scale with "n"
 
-//* Space; O(n) - The result array scales proportionally with the size of the input
-//* If the input is [10, 20, 30], that is length 3
-//* The result will be: [1, 1, 0], also length 3
+//* Space: O(n) - The answer array scales with the input size (n)
