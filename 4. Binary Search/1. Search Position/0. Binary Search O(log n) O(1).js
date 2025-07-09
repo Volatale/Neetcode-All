10b2bd930130d90fcdc30827ec2c7d3bdc915710
volatale <nathanamarirose@gmail.com>
2025-07-10 00:05:23 +0100

    Update Search Position

--- 4. Binary Search/1. Search Position/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/1. Search Position/0. Binary Search O(log n) O(1).js
@@ -1,35 +1,40 @@
-//* Don't remove the "mid" element from the search space
-//* Right starts at nums.length instead of nums.length - 1
-//* It is possible that you have an array like [1, 2, 3], and target = 4
-//* That means we need to return 3
-function searchInsertPosition(nums, target) {
+//* Given a SORTED array of distinct integers, we need to find "target"
+//* The goal is to return the index of target if it is found
+//* And if it isn't, return the index where it "would" be
+//* The input array exhibits monotonicity
+//*     - (nums[i] < nums[i + 1] < ... < nums[n-1])
+//* Additionally, the search space is the array itself (+ 1 because it could be the nth index)
+//* Thus, we can use a binary search approach to solve the problem
+//* In our case, we need to use "modified" binary search
+//* We can't always eliminate the current index
+function searchInsert(nums, target) {
   //* The search space is the array itself
   let left = 0;
   let right = nums.length;
 
   while (left < right) {
-    //* Mid represents the index of the element we are testing
-    let mid = left + ((right - left) >> 1);
+    //* The index we are searching
+    const mid = left + ((right - left) >> 1);
 
     if (nums[mid] >= target) {
-      right = mid; //* Don't eliminate mid, this could be our value
+      right = mid; //* Search the left
     } else {
-      left = mid + 1; //* Element is too small
+      left = mid + 1; //* Search the right
     }
   }
 
-  //* Index of the found element, or where it SHOULD be
+  //* The index of target, or the index it "would" be
   return left;
 }
 
-console.log(searchInsertPosition([1, 3, 5, 6], 5)); //* 2
-console.log(searchInsertPosition([0, 1, 2, 3], 4)); //* 4
-console.log(searchInsertPosition([1, 3, 4, 5], 2)); //* 1
-console.log(searchInsertPosition([1, 5, 7, 10], 4)); //* 1
-console.log(searchInsertPosition([1, 5, 7, 10], 2)); //* 1
-console.log(searchInsertPosition([1], 0)); //* 0
-console.log(searchInsertPosition([1], 2)); //* 1
+console.log(searchInsert([1, 3, 5, 6], 5)); //* 2
+console.log(searchInsert([0, 1, 2, 3], 4)); //* 4
+console.log(searchInsert([1, 3, 4, 5], 2)); //* 1
+console.log(searchInsert([1, 5, 7, 10], 4)); //* 1
+console.log(searchInsert([1, 5, 7, 10], 2)); //* 1
+console.log(searchInsert([1], 0)); //* 0
+console.log(searchInsert([1], 2)); //* 1
 
-//* Time: O(log n) - We halve the search space every iteration of the loop
+//* Time: O(log n) - Each iteration of the loop, we eliminate half of the search space
 
-//* Space: O(1) - No space usage scales with the input size
+//* Space: O(1) - The memory usage does not scale with the input size
