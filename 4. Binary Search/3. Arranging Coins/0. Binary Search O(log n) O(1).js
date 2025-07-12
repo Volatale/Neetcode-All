be855e8ba8a87c27a0ea030bde617ca6575899e5
volatale <nathanamarirose@gmail.com>
2025-07-12 01:49:51 +0100

    Update Arranging Coins

--- 4. Binary Search/3. Arranging Coins/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/3. Arranging Coins/0. Binary Search O(log n) O(1).js
@@ -1,43 +1,47 @@
-//* Arithmetic Series
-//! Total coins needed follows this pattern
-//*     - 1 + 2 + 3 + 4 + ... + n
-//* So we can technically binary search to optimize the choices
-//* [0...n]
-//* "mid" represents the number of rows we are trying to build
-//* If we don't have enough coins to complete the row
-//*     - Then try building LESS rows
-//* If we DO have enough coins, we have a potential best
-//*     - Then try building MORE rows
+//* We have `n` coins, and the goal is to build a staircase with the coins
+//* The staircase consists of `k` rows
+//*     - The ith row has exactly i coins
+//* The final row may be incomplete
+//* Given `n` coins, return the number of COMPLETE rows
+//* Immediately, we can observe that the "cost" to build a row increases by 1 each "construction"
+//*     - 1 + 2 + 3 + 4 + 5 + ... + n
+//*     - Thus, we can simply increment a variable every iteration
+//* Additionally, we can observe that the "costs" are monotonically increasing
+//* And the "correct" cost lies somewhere in the range [1, completeRows + 1]
+//! Thus, it is possible to binary search to optimize the search process
+//* The Sum of N Natural Numbers can tell us how many complete rows we can build with our current "mid" choice
+//*     - In this case, "mid" represents the number of rows we are trying to build
+//* If `n` < the result of the above, then we "can" build that many rows
+//* Otherwise, we need to try building a fewer amount of rows
 function arrangeCoins(n) {
-  //* The min and max no of rows we can try building
+  //* The search space is the range [1, n]
   let left = 1;
   let right = n;
 
   while (left < right) {
-    //* Mid represents the No. of Rows we are trying to build
+    //* Mid represents the number of rows we are trying to build
     const mid = left + ((right - left + 1) >> 1);
-
-    //* Coins needed to build "mid" rows
     const coinsNeeded = (mid * (mid + 1)) / 2;
 
-    if (coinsNeeded <= n) {
-      //* We have enough to build "mid" rows, so try more (greedily)
-      left = mid;
+    if (n >= coinsNeeded) {
+      left = mid; //* We're successful, so potentially try more rows
     } else {
-      //* We don't have enough coins; try building LESS rows
-      right = mid - 1;
+      right = mid - 1; //* We don't have enough coins; try a fewer amount
     }
   }
 
-  //* No. of Rows that can be built completely using "n" coins
+  //* No. of Complete Rows that can be built using `n` coins
   return left;
 }
 
-console.log(arrangeCoins(5)); //* 2
 console.log(arrangeCoins(1)); //* 1
-console.log(arrangeCoins(10)); //* 4
+console.log(arrangeCoins(2)); //* 1
+console.log(arrangeCoins(3)); //* 2
+console.log(arrangeCoins(5)); //* 2
 console.log(arrangeCoins(8)); //* 3
+console.log(arrangeCoins(10)); //* 4
 
-//* Time: O(log n) - Each iteration, we eliminate half of the search space
+//* Time: O(n) - The time taken scales with the input size
+//* Specifically, the Sum of N Natural Numbers
 
-//* Space: O(1) - The memory usage remains constant regardless of the input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
--- 4. Binary Search/3. Arranging Coins/2. Linear Search O(n) O(1).js
+++ 4. Binary Search/3. Arranging Coins/2. Linear Search O(n) O(1).js
@@ -1,24 +1,34 @@
-//* The number of coins w need is equal to rows + 1
-//* So you can continually create rows until this expression is false
-function arrangingCoins(n) {
-  let rows = 0;
+//* We have `n` coins, and the goal is to build a staircase with the coins
+//* The staircase consists of `k` rows
+//*     - The ith row has exactly i coins
+//* The final row may be incomplete
+//* Given `n` coins, return the number of COMPLETE rows
+//* Immediately, we can observe that the "cost" to build a row increases by 1 each "construction"
+//*     - Thus, we can simply increment a variable every iteration
+//* If we keep iterating, eventually we won't have enough coins to complete the row
+//* There is no need to keep iterating until we have 0 coins, however
+//*     - Instead, we simply check if the number of coins required for this row > remaining coins
+//*     - If that IS the case, we can't complete the row, so exit the loop
+function arrangeCoins(n) {
+  let completeRows = 0;
 
-  while (n >= rows + 1) {
-    n -= rows + 1;
-    rows++;
+  //* Keep iterating while we are able to complete the current row
+  while (n >= completeRows + 1) {
+    n -= completeRows + 1;
+    completeRows++;
   }
 
-  return rows;
+  return completeRows;
 }
 
-console.log(arrangingCoins(1)); //* 1
-console.log(arrangingCoins(2)); //* 1
-console.log(arrangingCoins(3)); //* 2
-console.log(arrangingCoins(5)); //* 2
-console.log(arrangingCoins(8)); //* 3
-console.log(arrangingCoins(10)); //* 4
+console.log(arrangeCoins(1)); //* 1
+console.log(arrangeCoins(2)); //* 1
+console.log(arrangeCoins(3)); //* 2
+console.log(arrangeCoins(5)); //* 2
+console.log(arrangeCoins(8)); //* 3
+console.log(arrangeCoins(10)); //* 4
 
-//* Time: O(n) - The time taken scales with "n"
-//* We do proportionally more iterations as "n" increases
+//* Time: O(n) - The time taken scales with the input size
+//* Specifically, the Sum of N Natural Numbers
 
-//* Space: O(1) - The space usage remains constant regardless of input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
