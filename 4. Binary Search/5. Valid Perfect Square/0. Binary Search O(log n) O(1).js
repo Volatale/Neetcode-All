b6454e2fe3cf780e8a178c086b903d7d2d84aa4c
volatale <nathanamarirose@gmail.com>
2025-07-14 01:46:14 +0100

    Update Valid Perfect Squares

--- 4. Binary Search/5. Valid Perfect Square/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/5. Valid Perfect Square/0. Binary Search O(log n) O(1).js
@@ -1,35 +1,38 @@
-//* The minimum square we can have is 1 (1 * 1)
-//* The largest possible square is num * num
-//* So our correct value lies somewhere within these bounds
-//* [1 .. num] is a sorted sequence of numbers
-//* Thus, this sequence can act as our search space for a binary search
-//* "mid" represents the current integer we are using as the test subject
-//* If mid * mid === num, "num" IS a perfect square
-//* Otherwise, adjust the search space in the correct direction
+//* Given a positive integer num, we need to return whether or not it is a perfect square
+//* A perfect square is an integer that is the SQUARE of an integer
+//*     - In other words, if the integer is `n`, the perfect square of `n` is (n * n)
+//* Square numbers (our search space) are monotonically increasing
+//* Since we have a search space, and the monotonic property, binary search is possible
+//* In our case, the minimum possible square root is 1, and the max is `n` itself
+//*     - We could initialize the right pointer to sqrt(n), but then the time complexity remains the same (O(sqrt(n)))
+//* if `mid * mid` === num, then we found the square root (return true)
+//* else if `mid * mid` > num, we need to try a SMALLER square root
+//* else, `mid * mid` < num, so we need to try a LARGER square root
 function isPerfectSquare(num) {
-  //* num's minimum value is 1, so the smallest square is 1 (1 * 1)
-  //* The largest possible square would be num * num, so know the bounds
+  //* The search space of the square roots is in the range [1, n]
   let left = 1;
   let right = num;
 
   while (left <= right) {
-    //* Mid represents the root of the number we want to test
-    let mid = left + ((right - left) >> 1);
+    //* Mid represents the square root we are testing
+    const mid = left + ((right - left) >> 1);
 
-    const square = mid * mid; //* Calculate the square of mid
+    const square = mid * mid;
 
     if (square === num) {
       return true;
     } else if (square > num) {
-      right = mid - 1; //* Square is too large
+      right = mid - 1; //* Try a smaller square root (to optimize)
     } else {
-      left = mid + 1; //* Square is too small
+      left = mid + 1; //* Try a larger square root
     }
   }
 
+  //* Exceptional circumstances
   return false;
 }
 
+console.log(isPerfectSquare(4)); //* True
 console.log(isPerfectSquare(16)); //* True (4 * 4)
 console.log(isPerfectSquare(14)); //* False
 console.log(isPerfectSquare(9)); //* True (3 * 3)
@@ -37,7 +40,6 @@ console.log(isPerfectSquare(121)); //* True (11 * 11)
 console.log(isPerfectSquare(8)); //* False
 console.log(isPerfectSquare(1)); //* True (1 * 1)
 
-//* Time: O(log n) - We halve the search space each iteration
-//* It takes O(1) to calculate the square
+//* Time: O(log n) - We are eliminating half of the search space every iteration
 
-//* Space: O(1) - The space we use remains constant regardless of the input
+//* Space: O(1) - The memory usage remains constant regardless of input size
--- 4. Binary Search/5. Valid Perfect Square/3. Square Root O(sqrt(n)) O(1).js
+++ 4. Binary Search/5. Valid Perfect Square/3. Square Root O(sqrt(n)) O(1).js
@@ -1,16 +1,23 @@
-//* "i" cannot exceed the square root of "num"
-//* i * i is the opposite way to check for a square root
-//* So if i * i eceeds num, you know "num" is not a perfect square
+//* Given a positive integer num, we need to return whether or not it is a perfect square
+//* A perfect square is an integer that is the SQUARE of an integer
+//*     - In other words, if the integer is `n`, the perfect square of `n` is (n * n)
+//! In a brute force manner, we can simply iterate from 1 up to (and including n)
+//* Just check if `i` (the current integer) multiplied by itself === n
+//* Why? We are trying to find the square root of num
+//*     - If there IS no integer square root of the input, then the input was not a perfect square
+//* We can also observe that since we are looking FOR the square root, we don't need to calculate beyond that
+//*     - If we are checking 9, then the only numbers we need to check are (1, 2, 3)
+//*     - 4 * 4 = 16 > 9, and the same is true for every number AFTER n
 function isPerfectSquare(num) {
-  for (let i = 1; i <= num; i++) {
-    const square = i * i;
-
-    if (square === num) {
+  //* Find the square root of num
+  for (let i = 1; i * i <= num; i++) {
+    if (i * i === num) {
       return true;
-    } else if (square > num) {
-      return false;
     }
   }
+
+  //* There is no square root for num
+  return false;
 }
 
 console.log(isPerfectSquare(16)); //* True (4 * 4)
@@ -20,11 +27,6 @@ console.log(isPerfectSquare(121)); //* True (11 * 11)
 console.log(isPerfectSquare(8)); //* False
 console.log(isPerfectSquare(1)); //* True (1 * 1)
 
-//* Time: O(sqrt(n)) - i * i is the opposite way to check for a square root
-//* If the square is larger than the square root of "n", it is invalid
+//* Time: O(sqrt) - We don't need to check BEYOND the largest possible square root
 
-//* Math.sqrt(1_000_000) = 1000
-//* log2(1_000_000) = 20, so log2(n) scales SLOWER than sqrt(n)
-//* So O(log n) is more efficient than O(sqrt(n))
-
-//* Space: O(1) - The space usage does not scale with the input
+//* Space: O(1) - The memory usage remains constant regardless of input size
