525d716b3240523fc956c4ece35a789879098105
volatale <nathanamarirose@gmail.com>
2025-07-15 08:14:42 +0100

    Update Sqrt(x)

--- 4. Binary Search/6. Sqrt(x)/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/6. Sqrt(x)/0. Binary Search O(log n) O(1).js
@@ -1,44 +1,51 @@
-//* Search space starts at 1, ends at x
-//* The value we are looking for exists within these bounds
-//* If mid * mid > x, then we need to decrease the search space on the right
-//* Otherwise, narrow it on the left
-//* In cases like sqrt(8), we need to subtract 1, because left would be 3
-//* 3 - 1 = 2
-function sqrtX(x) {
-  //* 0 * 0 = 0, 1 * 1 = 1
-  if (x <= 1) return x;
-
-  //* The search space is from 1 to x
+//* Given a non-negative integer x, we need to return the square root of x
+//*     - The return value should be rounded down to the nearest integer (floor it)
+//* Ultimately, we can iterate upward from 0 up to the input value
+//* However... This is slow, and we can make observations to avoid this process
+//* We know that the square roots fall into a monotonically increasing sequence
+//* And we ALSO know that we are trying to find the square root (so this is our search space)
+//* Thus, we can apply binary search to optimize our search process
+//* Calculate the square of each possible "mid" value we get
+//*     - If square > x, then we try a smaller square root
+//*     - Else, square < x, so we try a larger square root
+//! We take the else path EVEN in cases where we have the correct square root
+//* Why? Because we need to handle the case like sqrt(8), which is something like 2.82
+//* Since our "mid" will always be an integer (whole number), we go ONE beyond what the correct result is
+//* And then we subtract 1. So, we exit the loop when (left === right === 3)
+//*     - Then, 3 - 1 = 2, which is the result of Math.floor(sqrt(8))
+function mySqrt(x) {
+  //* The search space is the range of all possible square roots
   let left = 1;
   let right = x;
 
   while (left < right) {
-    //* "Mid" represents the root of the square we are trying to make
-    let mid = left + ((right - left) >> 1);
+    //* Mid is the square root that we are testing
+    const mid = left + ((right - left) >> 1);
 
-    //* If mid * mid === x, you found the square root
-    if (mid * mid > x) {
-      right = mid; //* Eliminate right, but not the mid element
+    const square = mid * mid;
+
+    if (square > x) {
+      right = mid; //* Try a smaller square root
     } else {
-      left = mid + 1; //* Eliminate Left
+      left = mid + 1; //* We need a larger square root
     }
   }
 
-  //* "left" is always 1 greater than what we need, so subtract 1, handles every case
+  //* "left" is ALWAYS one greater than what we need, so subtract 1
   return left - 1;
 }
 
-console.log(sqrtX(0)); //* 0
-console.log(sqrtX(1)); //* 1
-console.log(sqrtX(2)); //* 1
-console.log(sqrtX(4)); //* 2
-console.log(sqrtX(7)); //* 2
-console.log(sqrtX(8)); //* 2
-console.log(sqrtX(9)); //* 3
-console.log(sqrtX(121)); //* 11
-console.log(sqrtX(100)); //* 10
-console.log(sqrtX(16)); //* 4
+console.log(mySqrt(0)); //* 0
+console.log(mySqrt(1)); //* 1
+console.log(mySqrt(2)); //* 1
+console.log(mySqrt(4)); //* 2
+console.log(mySqrt(7)); //* 2
+console.log(mySqrt(8)); //* 2
+console.log(mySqrt(9)); //* 3
+console.log(mySqrt(121)); //* 11
+console.log(mySqrt(100)); //* 10
+console.log(mySqrt(16)); //* 4
 
-//* Time: O(log n) - We halve the search space each iteration
+//* Time: O(log n) - We halve the search space every iteration, so the time complexity is logarithmic (base 2)
 
-//* Space: O(1) - The space we use remains constant regardless of the input
+//* Space: O(1) - The memory usage remains constant regardless of input size
--- 4. Binary Search/6. Sqrt(x)/2. Square of i O(sqrt(n)) O(1).js
+++ 4. Binary Search/6. Sqrt(x)/2. Square of i O(sqrt(n)) O(1).js
@@ -1,9 +1,14 @@
-//* Try squaring every element from 1 to x (inclusive)
-//* If i * i > x, then this the result would be too large
-function sqrtX(x) {
+//* Given a non-negative integer x, we need to return the square root of x
+//*     - The return value should be rounded down to the nearest integer (floor it)
+//* Ultimately, we can iterate upward from 0 up to the input value
+//* Simply calculate "i * i" where "i" is the current integer
+//! An observation we can make is that we know for sure that the square root exists
+//* Thus, there is no need to try every possibility below the input itself
+//* Keep iterating while (i * i) <= x
+//*     - We know for sure this won't cause any issues
+function mySqrt(x) {
   let result = 0;
 
-  //* Try every integer until i * i > x
   for (let i = 1; i * i <= x; i++) {
     result = i;
   }
@@ -11,19 +16,17 @@ function sqrtX(x) {
   return result;
 }
 
-console.log(sqrtX(0)); //* 0
-console.log(sqrtX(1)); //* 1
-console.log(sqrtX(2)); //* 1
-console.log(sqrtX(4)); //* 2
-console.log(sqrtX(7)); //* 2
-console.log(sqrtX(8)); //* 2
-console.log(sqrtX(9)); //* 3
-console.log(sqrtX(121)); //* 11
-console.log(sqrtX(100)); //* 10
-console.log(sqrtX(16)); //* 4
+console.log(mySqrt(0)); //* 0
+console.log(mySqrt(1)); //* 1
+console.log(mySqrt(2)); //* 1
+console.log(mySqrt(4)); //* 2
+console.log(mySqrt(7)); //* 2
+console.log(mySqrt(8)); //* 2
+console.log(mySqrt(9)); //* 3
+console.log(mySqrt(121)); //* 11
+console.log(mySqrt(100)); //* 10
+console.log(mySqrt(16)); //* 4
 
-//* Time: O(sqrt(n)) - The time taken scales with the sqrt(n)
-//* i * i gives you the square of "i"
-//* If you go BEYOND x, then you know any number greater is useless
+//* Time: O(sqrt(n)) - We don't need to do more than sqrt(n) iterations
 
-//* Space: O(1) - We use a constant amount of space regardless of the input
+//* Space: O(1) - The memory usage remains constant regardless of input size
