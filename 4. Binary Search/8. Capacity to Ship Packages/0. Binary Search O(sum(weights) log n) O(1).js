9c101d4ec00d960f1b4b5602711d60ee84d2f30e
volatale <nathanamarirose@gmail.com>
2025-07-17 23:02:14 +0100

    Update Capacity to Ship Packages

--- 4. Binary Search/8. Capacity to Ship Packages/0. Binary Search O(sum(weights) log n) O(1).js
+++ 4. Binary Search/8. Capacity to Ship Packages/0. Binary Search O(sum(weights) log n) O(1).js
@@ -1,61 +1,69 @@
-//* "Days" is essentially another way to say how many trips we can make
-//* We want to try shipping using certain capacities until we are successful
-//* Try to find the "lowest" capacity that we can use while still shipping all packages within "days" days
-
-//* Imagine a world where we have ONE package [10] to ship in ONE day (highest weight)
-//* Then imagine a world where we have to ship ALL the packages in ONE day (sum)
-//* Left = Highest weight package
-//* Right = Sum of all packages
-function capacityToShipPackages(weights, days) {
+//* Packages must be shipped within `days` days (constraint)
+//*     - The `ith` package has a weight of weights[i]
+//* Each `day`, packages are loaded in the order given by weights
+//! We cannot load more weight than the maximum capacity of the ship
+//* The goal is to return the LEAST weight capacity that still allows us to make the shipment
+//*     - This implies optimization
+//* Instead of using a brute force approach, we can make some observations
+//! We are trying to find the "optimal" (minimum) capacity
+//* The capacities are technically sorted in ascending order (monotonic property)
+//* Since we want to optimize, and the search space is "sorted"...
+//*     - We can apply Binary Search
+//* "mid" represents the current capacity we are going to try
+//* If we manage to ship within "d" days, then we know at the very least, this "could" be the minimum capacity
+//* Otherwise, we need a higher capacity ship
+//* The minimum possible capacity is the largest value in the weights array
+//*     - Imagine we could only ship a single package every day
+//* The maximum possible capacity is the sum of all values in the weights array
+//*     - Imagine we had to ship all of the packages within a single day
+//*     - The ship must be able to hold the weight of ALL of the packages at once
+function shipWithinDays(weights, days) {
   function canShip(capacity) {
-    let daysPassed = 1; //* We start on the first day
+    let day = 1;
     let totalWeight = 0;
 
-    //* Try to ship all of the packages
-    for (let i = 0; i < weights.length; i++) {
-      totalWeight += weights[i]; //* Add the package to the ship
+    for (let weight of weights) {
+      //* Greedily assume the ship is big enough
+      totalWeight += weight;
 
-      //* Ship has to make multiple trips
+      //* Then, if it wasn't, this package is shipped on the next day
       if (totalWeight > capacity) {
-        daysPassed++;
-        totalWeight = weights[i];
+        totalWeight = weight;
+        day++;
       }
-
-      //* Can't ship all the packages in time with this capacity
-      if (daysPassed > days) return false;
     }
 
-    return true;
+    //* If this is true, the shipment was successful
+    return day <= days;
   }
 
-  //* If you have to ship a weight of just [10] in ONE day, the minimum has to be 10
-  //* Shipping the packages in ONE day means the minimum capacity has to be the sum of all
+  //* The search space is the range of possible capacities
   let left = Math.max(...weights);
   let right = weights.reduce((acc, curr) => acc + curr, 0);
 
   while (left < right) {
-    //* Mid = "capacity" that we want to try to ship all the packages using
-    let mid = left + ((right - left) >> 1);
+    //* Mid represents the current capacity we are trying
+    const mid = left + ((right - left) >> 1);
 
     if (canShip(mid)) {
-      //* Any capacity larger is useless now (but don't eliminate mid, it was SUCCESSFUL)
-      right = mid;
+      right = mid; //* We were successful; don't exclude the current capacity
     } else {
-      left = mid + 1; //* Unable to ship with this capacity, try larger
+      left = mid + 1;
     }
   }
 
+  //* The minimum possible capacity
   return left;
 }
 
-console.log(capacityToShipPackages([2, 3, 5], 2)); //* 5
-console.log(capacityToShipPackages([2, 3, 5, 6], 2)); //* 10
-console.log(capacityToShipPackages([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)); //* 15
-console.log(capacityToShipPackages([3, 2, 2, 4, 1, 4], 3)); //* 6
-console.log(capacityToShipPackages([1, 2, 3, 1, 1], 4)); //* 3
+console.log(shipWithinDays([2, 3, 5], 2)); //* 5
+console.log(shipWithinDays([2, 3, 5, 6], 2)); //* 10
+console.log(shipWithinDays([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)); //* 15
+console.log(shipWithinDays([3, 2, 2, 4, 1, 4], 3)); //* 6
+console.log(shipWithinDays([1, 2, 3, 1, 1], 4)); //* 3
 
-//* Time: O(sum(weights) * log n) - We are doing a binary search on the search space (O(log n))
-//* Within each binary search, we do an O(n) iteration to attempt to ship all the packages
-//* In the worst case, we iterate over every element in the input (a successful operation)
+//* Time: O(sum(weights) log n) - The range of possible capacities is in the range 1 to sum(weights)
+//* We eliminate half of the search space every iteration, so the while loop is logarithmic
+//* However, it takes O(n) to check if the capacity is enough
 
-//* Space: O(1) - We don't use any space that scales with the input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
