cf63b0846e674516870a7fd50b3898e892317dd1
volatale <nathanamarirose@gmail.com>
2025-07-18 00:10:09 +0100

    Update Find Peak Element

--- 4. Binary Search/9. Find Peak Element/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/9. Find Peak Element/0. Binary Search O(log n) O(1).js
@@ -1,35 +1,34 @@
-//* The intuition to use Binary Search comes from multiple factors
-//* We are given the constraint "nums[i] != nums[i + 1] for all valid i."
-//* This means you don't end up in a situation like [1, 1, 1]
-//* One crucial element of binary search is to ALWAYS have a direction to search in
-//* In THIS problem, we don't have a "general" monotonic element, but we have a "local" one
-//* At each index, since we KNOW the left and right are NOT equal to the current element
-//* They have to be different, so we should favor the direction of the LARGER element (than nums[i])
-//* The problem states that index 0 and index n - 1 are strictly greater than the side with no neighbors
-//* So even in a situation like: [5, 4, 3, 2, 1], we can travel to any corner and find a peak
-//* 5 > 4 (on the right side), 5 > out of bounds (on the left side), so we found a peak element
-//* We are allowed to return ANY peak element, and the array can have multiple
-//* Even if the array was [1, 2, 3, 4, 5], the same applies (keep travelling right, which is the larger element)
-//* [1, 2, 3, 4, 2], here, the peak element is "4" (index 3), we traveled right, and converged on index 3
+//* A `peak` element is said to be strictly greater than its neighbors
+//* Given an integer array, we need to return any peak that exists in the array
+//*     - There can be multiple peaks in the same input
+//! Out of bounds is always considered -Infinity (so in-bounds elements are always greater)
+//* One observation we can make is that if we travel toward the larger elements, we will always find the peak
+//* Either the element will be greater than both its neighbors
+//* Or, it'll be greater than ONE of its neighbors, and the other element is out of bounds
+//* We are searching for something within the array, so our search space is the array itself/
+//*     - Specifically, the array indices, which are monotonically increasing
+//* There is also no problem with duplicate elements; we can return ANY of the potential peaks
+//*     - So we continuously move toward ANY of the larger elements
+//* If nums[mid] > nums[mid + 1], then we are already at the larger element
+//*     - right = mid (don't eliminate this element from the search space)
+//* Otherwise, a larger element exists in the right partition somewhere
+//*     - left = mid + 1 (eliminate the current element)
 function findPeakElement(nums) {
   //* The search space is the array itself
   let left = 0;
   let right = nums.length - 1;
 
-  //* Both left and right converge on a peak element
   while (left < right) {
-    //* Mid is the element we are testing for peak
-    let mid = left + ((right - left) >> 1);
+    //* Mid represents the element we are checking for "peak" property
+    const mid = left + ((right - left) >> 1);
 
-    //* Travel in the direction of the side with the larger element
-    if (nums[mid] > nums[mid + 1]) {
-      right = mid; //* Mid element could be a potential peak
+    if (mid + 1 === nums.length || nums[mid] > nums[mid + 1]) {
+      right = mid; //* Search the left partition
     } else {
-      left = mid + 1; //* Larger value is on the left
+      left = mid + 1; //* Search the right partition
     }
   }
 
-  //* Index of a peak element
   return left;
 }
 
@@ -41,6 +40,6 @@ console.log(findPeakElement([1, 2, 3, 7])); //* 3 (7)
 console.log(findPeakElement([55, 66, 67, 68, 69])); //* 4 (69)
 console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4])); //* 5 (6)
 
-//* Time: O(log n) - We halve the search space each iteration of the loop
+//* Time: O(log n) - We halve the search space each iteration, so the time taken is logarithmic with respect to `n`
 
-//* Space: O(1) - The space usage does not scale with the input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
