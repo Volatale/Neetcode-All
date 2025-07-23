c732ae20d847df7f119ed60c9f7a2a5e023c9c77
volatale <nathanamarirose@gmail.com>
2025-07-23 23:28:58 +0100

    Update Min the Max Difference of Pairs

--- 4. Binary Search/13. Minimize the Maximum Difference of Pair/0. Binary Search O(n log n + p log m) O(n).js
+++ 4. Binary Search/13. Minimize the Maximum Difference of Pair/0. Binary Search O(n log n + p log m) O(n).js
@@ -1,59 +1,60 @@
-//* Sort the array to guarantee that the elements are close together in value
-//* This allows us to MINIMIZE the difference between each potential pair
-//* By sorting, we introduce a monotonic element to the problem
-//* The search space is the range of potential differences we can have
-//* So that ranges from 0 (because the minimum value is 0), and max value - min value
-//* "mid" represents the maximum difference that we are testing
-//* If we are able to make "p" pairs with THIS maximum, then "mid" is a potential value
-//* Since we sorted the array, [3, 4] would be |3 - 4| = 1, but 4 - 3 ALSO results in 1
-//* So we don't HAVE to use absolute values anymore
-function minimizeMaximumDifference(nums, p) {
-  function canMakePairs(maxDiff) {
+//* We are given an int[] `nums` and an integer `p`
+//* The goal is to find `p` pairs of indices in nums such that the max difference is minimized (among pairs)
+//*     - Each index can only appear once amongst the `p` pairs
+//*     - The difference of the pair is the absolute value of |pairs[i] - pairs[j]|
+//* Return the MINIMUM maximum difference among all `p` pairs
+//* By sorting the array we ensure the array exhibits monotonicity
+//* It also makes it easier to find the minima and maxima
+//*     - nums[0] - nums[n-1] is the maximum possible difference
+//*     - And any potential pair inbetween that could be the minimum
+//* Since the array is sorted, we can apply a binary search approach
+//*     - Left = 0 (minimum)
+//*     - Right = nums[n-1] - nums[0]
+//*     - Mid repesents the CURRENT maximum distance we are testing
+//* If we are able to form `p` pairs using this mid, then we continue (search the left and retain the current mid)
+//* Else, we are unable to form `p` pairs, so we need a larger mid (search the right and eliminate the current mid)
+function minimizeMax(nums, p) {
+  function canMakePairs(maxDifference) {
     let pairs = 0;
 
-    //* We are told | nums[i] - nums[i + 1] | = the difference
-    //* But we ALSO know that the elements are sorted now
-    //* [3, 4]: abs(3 - 4) = 1, but 4 - 3 = 1 too, so it works either way
-    for (let i = 1; i < nums.length && pairs < p; i++) {
-      if (nums[i] - nums[i - 1] <= maxDiff) {
+    for (let i = 1; i < nums.length && pairs <= p; i++) {
+      if (nums[i] - nums[i - 1] <= maxDifference) {
         pairs++;
-        i++; //* We paired with the previous element, so don't try this pair again
+        i++; //* Avoid reusing the same element (index) again
       }
     }
 
-    //* Can we successfully make p pairs?
+    //* If true, then we search the left portion
     return pairs >= p;
   }
 
-  //* We want to minimize the difference, so sorting means elements are close in value
+  //* Sort the array into non-monotonically decreasing order
   nums.sort((a, b) => a - b);
 
-  //* Search space is from 0 to the max - min value (after sorting)
+  //* The search space is the range of possible differences
   let left = 0;
   let right = nums[nums.length - 1] - nums[0];
 
   while (left < right) {
-    //* Mid represents the maximum difference we are trying
-    let mid = left + ((right - left) >> 1);
+    //* `mid` represents the current "maximum difference" we are testing
+    const mid = left + ((right - left) >> 1);
 
     if (canMakePairs(mid)) {
-      right = mid; //* Mid was successful; don't eliminate it from the search space
+      right = mid; //* We were successful; optimize by searching the left
     } else {
-      left = mid + 1; //* Mid failed, find a larger max difference
+      left = mid + 1; //* We failed; try a higher maximum difference
     }
   }
 
-  //* Maximum difference between all pairs
   return left;
 }
 
-console.log(minimizeMaximumDifference([10, 1, 2, 7, 1, 3], 2)); //* 1
-console.log(minimizeMaximumDifference([4, 2, 1, 2], 1)); //* 0
-console.log(minimizeMaximumDifference([3, 7, 6, 4, 10, 5], 3)); //* 3
+console.log(minimizeMax([10, 1, 2, 7, 1, 3], 2)); //* 1
+console.log(minimizeMax([4, 2, 1, 2], 1)); //* 0
+console.log(minimizeMax([3, 7, 6, 4, 10, 5], 3)); //* 3
 
-//* Time: O(n log n + p log m)) - Where "m" is the max(nums)
-//* We sort the input, which takes O(n log n) time
-//* Then, we do a binary search whose search space scales based on [0 .. max(nums) - nums[0]]
-//* Within each binary search iteration, we do an O(n) for loop, but this is dominated by the n log ns
+//* Time: O(n log n + n log r) - Where `n` is the length of the array (.sort)
+//* The range of possible max differences is nums[n-1] - nums[0] (r)
+//* Within each while loop iteration, we perform an O(n) loop in the worst case
 
-//* Space: O(n) - The inbuilt sorting algorithm probably uses merge sort, so O(n) space
+//* Space: O(sort) - The memory used scales with the sorting algorithm used
