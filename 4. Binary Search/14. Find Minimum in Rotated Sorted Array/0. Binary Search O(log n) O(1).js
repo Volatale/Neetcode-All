501fffbc247b7402fb17e32d0542b0a7751468dd
volatale <nathanamarirose@gmail.com>
2025-07-24 01:07:53 +0100

    Update Find Minimum in Rotated Sorted Array

--- 4. Binary Search/14. Find Minimum in Rotated Sorted Array/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/14. Find Minimum in Rotated Sorted Array/0. Binary Search O(log n) O(1).js
@@ -1,36 +1,33 @@
-//! Once rotated, the array is at least PARTIALLY sorted
-//* An O(log n) time complexity means we need to use binary search
-//! The search space is the array itself
+//* We have an array of length `n` that has been rotated between 1 and `n` times
+//* The goal is to return the MINIMUM element in the sorted array
+//* We can make some observations:
+//*     - The array is sorted (exhibits monotonicity)
+//*     - All of the elements are unique
+//*     - We are searching for something within the array itself
+//*         - i.e. our search space is the array
+//* We can apply binary search here and eliminate the majority of the possiblities in each iteration
+//* The search space is the array itself
 //*     - left = 0
 //*     - right = nums.length - 1
-//* [1, 2, 3, 4, 5], rotate twice
-//* [3, 4, 5, 1, 2]
 //* If nums[mid] > nums[right]
-//*     - Then we are on the wrong side
-//*     - We need to move to the right to find the SMALLER elements
-//* [1, 2, 3, 4, 5], rotated 5 times
-//* if nums[mid] > nums[left]
-//*     - nums[right] > nums[left]
-//*         - So don't go to the right
-//*     - nums[mid] > nums[left]
-//*         - So we should probably travel to the left
-//! Determine what side "mid" is on
-//*     - It is either on the "smaller" side
-//*     - Or it is on the "larger" side
+//*     - Then we know there is a monotonically increasing subarray to our right
+//*     - left = mid + 1
+//* Otherwise, there is one to our left
+//*     - Search the left
+//*     - right = mid
 function findMin(nums) {
   //* The search space is the array itself
   let left = 0;
   let right = nums.length - 1;
 
   while (left < right) {
-    //* "Mid" is the index of the potential smallest element
+    //* Mid represents the current index we are searching
     const mid = left + ((right - left) >> 1);
 
-    //* If true, then "mid" is on the LARGER portion of the array
     if (nums[mid] > nums[right]) {
-      left = mid + 1; //* Move to the SMALLER portion
+      left = mid + 1; //* The minimum exists in the right subarray (partition)
     } else {
-      right = mid; //* The smaller portion is on the left
+      right = mid; //* We either found the minimum or it exists in the left subarray (partition)
     }
   }
 
@@ -45,6 +42,6 @@ console.log(findMin([20])); //* 20
 console.log(findMin([1, 30])); //* 1
 console.log(findMin([4, 5, 6, 7, 0, 1, 2])); //* 0
 
-//* Time: O(log n) - We are eliminating half of the search space every iteration
+//* Time: O(log n) - The search space (n) is halved within each iteration
 
-//* Space: O(1) - The memory usage remains constant regardless of the input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
