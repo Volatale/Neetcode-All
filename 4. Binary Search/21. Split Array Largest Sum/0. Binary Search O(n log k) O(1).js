7e2f07fa1835627149f633f33a81f6606bee6257
volatale <nathanamarirose@gmail.com>
2025-08-01 07:30:58 +0100

    Update Split Array Largest Sum

--- 4. Binary Search/21. Split Array Largest Sum/0. Binary Search O(n log k) O(1).js
+++ 4. Binary Search/21. Split Array Largest Sum/0. Binary Search O(n log k) O(1).js
@@ -1,60 +1,63 @@
-//* This is an optimization problem (find the MINIMUM successful candidate)
-//* So we have a predicate to test, and something to work towards (the minimum)
-//* The MINIMUM sum a subarray can hold is the max(nums)
-//* Imagine if we had [10, 2], and K = 2, the minimized largest sum is 10 in that case
-//* The MAXIMUM sum an array can hold is every element at once
-//* [10, 20, 30], k = 1; every element has to fit into ONE subarray, so the sum is 60
-//* If we take "mid" to represent the maxSum that we want to test, we don't have to test every possibility
-//* Instead, we can just binary search to narrow down our options
-function splitArrayLargestSum(nums, k) {
+//* We are given an int[] `nums` and an int `k`
+//* The goal is to split `nums` to `k` non-empty subarrays while MINIMIZING the sum of each subarray
+//* Then we need to return the minimized largest sum of the split
+//*     - Thus, minimise the sum of every subarray, and then return the largest sum of them all
+//* This is an optimization problem that involves partitioning
+//* So binary search may be applicable; we are searching for the optimal spots to split
+//* We can compare splits by summing the array and using an absolute difference
+//* The splits that result in the LEAST absolute difference are the optimal splits
+//* We are trying to minimize the SUM of each split, so our `mid` can represent the MAX sum we are testing
+//*     - if(!canSplit) -> right = mid
+//*         - Success, we found a valid candidate, but we still want the smallest
+//*     - if(canSplit) -> left = mid + 1
+//*         - Failure, we need a larger sum per subarray
+//* As for the splitting, the splits happen left to right, so we can just simulate it mathematically
+//*     - If the sum of a subarray > maxSum (k), simply start the count again (simulates a new array)
+//*     - Increment the number of "subarrays" that would be created
+function splitArray(nums, k) {
   function canSplit(maxSum) {
     let sum = 0;
-    let subarrays = 1; //* We start with a subarray
+    let subarrays = 1;
 
-    for (let i = 0; i < nums.length; i++) {
-      sum += nums[i]; //* Add the element to this "array"
+    //* Greedily add each element to a subarray from left-to-right
+    for (let i = 0; i < nums.length && subarrays <= k; i++) {
+      sum += nums[i];
 
-      //* This means we have to create a NEW subarray
+      //* If an overflow occurs, then begin a "new" subarray
       if (sum > maxSum) {
-        //* Start a new subaray with this element (couldn't fit into the other)
         sum = nums[i];
         subarrays++;
-
-        //* You can't create more than K subarrays
-        if (subarrays > k) return false;
       }
     }
 
-    //* Successfully split into <= k subarrays with this maxSum
-    return true;
+    return subarrays <= k;
   }
 
-  //* If K = 1, and nums = [10], the minimum the array can be is 10 (max(nums))
-  //* At worst, every element needs to fit into ONE array, so take the sum of everything
+  //* The search space is the range of sums [max(nums), sum(nums)]
   let left = Math.max(...nums);
-  let right = nums.reduce((sum, curr) => (sum += curr), 0);
+  let right = nums.reduce((acc, curr) => acc + curr, 0);
 
   while (left < right) {
-    //* Mid represents the "maxSum" that each array can have
-    let mid = left + ((right - left) >> 1);
+    //* `mid` represents the current "maxSum" we are testing
+    const mid = left + ((right - left) >> 1);
 
     if (canSplit(mid)) {
-      right = mid; //* Found a new best, don't eliminate mid
+      right = mid; //* Found a new min candidate, don't eliminate it
     } else {
-      left = mid + 1; //* Creates too many arrays, increase mid
+      left = mid + 1; //* We created too many arrays, need a larger sum
     }
   }
 
+  //* The minimized maximum sum of the optimal splits
   return left;
 }
 
-console.log(splitArrayLargestSum([1, 2, 3, 4, 5], 1)); //* 10
-console.log(splitArrayLargestSum([1, 2, 3, 4, 5], 2)); //* 9
-console.log(splitArrayLargestSum([7, 2, 5, 10, 8], 2)); //* 18
+console.log(splitArray([7, 2, 5, 10, 8], 2)); //* 18
+console.log(splitArray([1, 2, 3, 4, 5], 1)); //* 15
+console.log(splitArray([1, 2, 3, 4, 5], 2)); //* 9
 
-//* Time: O(n log k) - Where "k" is the sum of nums
-//* We do a binary search on the range of maxSum we can have
-//* Ranging from max(nums) to sum(nums)
-//* Within each binary search, we do an O(n) iteration (in the worst case)
+//* Time: O(n log(r)) - Where `n` is nums.length and `r` is the following range [max(nums), sum(nums)]
+//* Within each while loop iteration, we do O(n) work to check for the split validity
+//* The total search space is halved within each iteration
 
-//* Space: O(1) - We only use constant space variables
+//* Space: O(1) - The memory usage remains constant regardless of input size
