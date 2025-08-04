42ff260da91ffe51fdb2bed5fbc1aa72c6c79516
volatale <nathanamarirose@gmail.com>
2025-08-05 00:16:50 +0100

    Update First Bad Version

--- 4. Binary Search/25. First Bad Version/0. Binary Search O(log n) O(1).js
+++ 4. Binary Search/25. First Bad Version/0. Binary Search O(log n) O(1).js
@@ -1,33 +1,33 @@
-//* We know the first version is 1
-//* And we also know the last version released was "n"
-//* The range of versions is [1, 2, .. n]. A SORTED sequence of numbers
-//* The task involves FINDING the FIRST element within this SEARCH SPACE
-//* It is also important to MINIMIZE the number of calls (checks)
-//* There exists a point in the sequence where the bad version exists
-//* Any version BEFORE that is "GOOD", and anything AFTER (including that version) is bad
-//* So the search space contains monotonocity
-//* Using all of the above information, we can use Binary Search
-function firstBadVersion(n) {
-  //* First version is 1, last version is n
-  //* Return value exists within this search space
+//* We are told we need to find the "first bad version"
+//* A "bad" version in this case is a number that returns "true" when a function is called
+//* There exists a series of "good" versions, followed by a series of "bad" versions
+//*     - After the first "bad" version, every SUBSEQUENT version is ALSO bad
+//! This implies the existence of a monotonic property
+//* Our goal is to find the point at which every subsequent version is bad
+//* And we also need to reduce the search space as much as possible every iteration
+//! Thus, we should use a binary search approach
+//* The range of possible versions is given as [1, n] (inclusive)
+//*     - The two pointers should converge to a single point, which tells us the "first bad version"
+function solution(isBadVersion) {
+  //* The search space is the range of possibe versions [1, n]
   let left = 1;
   let right = n;
 
-  //* Left and right eventually find the same element
   while (left < right) {
-    let mid = left + ((right - left) >> 1); //* mid = the version we want to test
+    //* `mid` represents the version we are currently checking
+    const mid = left + ((right - left) >> 1);
 
     if (isBadVersion(mid)) {
-      right = mid; //* Don't eliminate "mid", it could be the FIRST bad version
+      right = mid; //* Potential candidate; don't eliminate it from the search space
     } else {
-      left = mid + 1; //* Mid was useless, so eliminate left portion
+      left = mid + 1; //* Found a good version, eliminate it from the search space
     }
   }
 
-  //* The first bad version
+  //* Left indicates the first bad version
   return left;
 }
 
-//* Time: O(log n) - We halve the search space every iteration of the loop
+//* Time: O(log n) - The search space is halved each iteration
 
-//* Space: O(1) - The space usage remains constant regardless of "n"
+//* Space: O(1) - The memory usage remains constant regardless of input size
