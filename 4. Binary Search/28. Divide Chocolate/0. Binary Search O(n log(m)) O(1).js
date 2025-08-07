c52a8adb365a5c18760e6c4efba6dd22acb9dc18
volatale <nathanamarirose@gmail.com>
2025-08-08 00:01:12 +0100

    Update Divide Chocolate

--- 4. Binary Search/28. Divide Chocolate/0. Binary Search O(n log(m)) O(1).js
+++ 4. Binary Search/28. Divide Chocolate/0. Binary Search O(n log(m)) O(1).js
@@ -1,47 +1,69 @@
-//* The maximum amount of cuts we can make is "k"
-//* So if the number of cuts < maxCuts, we can successfully distribute the chocolate
-//* Take "mid" to represent the maxSweetness that each block should have
-//* We can be GREEDY, however, so maxSweetness of 5 still works with [1, 2, 5]
-//* The "5" piece is just added to the previous block
-//* Don't eliminate the "mid" value if we are successful, this could be the return value
-//* We aren't searching for a value WITHIN the array itself, so we don't do left <= right
+//* We are given chunks of chocolate whose sweetness is given by the int[] `sweetness`
+//* The goal is to share the chocolate with `k` friends so we cut the chocolate bar into `k + 1` pieces (k cuts)
+//* "We" eat the piece of the MINIMUM total sweetness and give the other pieces to our friends
+//* Then, we need to find the MAXIMUM total sweetness we can get by cutting optimally
+//! In other words, maximize the minimum
+//* We know that the chocolate bar pieces are "consecutive chunks", which implies that they are subarrays
+//*     - "Total sweetness" implies that we need to sum the values in each subarray
+//* Since we are trying to do something "optimally", we have a range of possibilities
+//* Regardless of our choice, there must be `k + 1` subarrays at the end of each
+//*     - This implies that `k` < sweetness.length
+//*         - if sweetness.length = 5, then k = [0, 4]
+//*         - Because there must be `k + 1` (5) subarrays at the end, which means each subarray holds a single value
+//*     - Why? Because there must be `k + 1` subarrays at the end
+//*         - If `n` = 5, and `k` = 6, then there must be 7 subarrays after each split, which is impossible
+//*         - There aren't enough pieces to cut up
+//* In our case we want to "optimize" the total sweetness (over each of the chunks)
+//! The values exists in the range [max(sweetness), sum(sweetness)]
+//*     - imagine we have [1, 2, 3], k = 2
+//*         - Here, we can only have single element subarrays: [1], [2], [3] (k + 1 = 3 subarray)
+//*         - So the minimum sweetness value is then therefore min(sweetness)
+//*     - Alternatively, we could have [1, 2, 3], k = 0
+//*         - Which means we have [1, 2, 3] for a subarray (k + 1 = 1 subarray)
+//*         - The maximum sweetness would then have to be then sum of all elements in the subarray
+//* Since we know we have a "range" of possible sweetnesses, that is our search space
+//* We also know we need to optimize something (the maximnum sweetness that should exist per subarray)
+//! Thus, binary search will work perfectly here
+//*     - left = min(sweetness)
+//*     - right = sum(sweetness)
+//*     - `mid` represents the maximum sweetness we will allow per subarray
 function divideChocolate(sweetness, k) {
-  function sweetnessValid(maxSweetness) {
-    let maxCuts = k + 1;
-    let cuts = 0; //* Track the number of cuts we do
-    let totalSweetness = 0; //* Sum the sweetness
+  function canSplit(maxSweetness) {
+    const maxCuts = k + 1;
+    let cuts = 0;
+    let totalSweetness = 0; //* Sweetness of current subarray
 
-    //* Try cutting the chocolate bar
-    for (let sweet of sweetness) {
-      totalSweetness += sweet;
+    for (let i = 0; i < sweetness.length && cuts < maxCuts; i++) {
+      //* Add current chunk to current subarray (greedily)
+      totalSweetness += sweetness[i];
 
-      //* Each block has to have <= maxSweetness "sweetness" (be GREEDY)
+      //* If sweetness is too much, add this chunk to the next subarray
       if (totalSweetness > maxSweetness) {
         cuts++;
-        totalSweetness = 0; //* THIS sweet has been placed into the current cut
+        totalSweetness = 0;
       }
     }
 
-    //* True means we can try to decrase the max sweetness
+    //* There must be exactly k + 1 subarrays at the end
     return cuts < maxCuts;
   }
 
-  //* The minimum sweetness is 1
-  //* If we have an input like [1, 5, 10, 50, 20], the block HAS to contain every element (sum of all)
-  let left = 1;
+  //* Our search space is the range of possible sweetness values
+  let left = Math.min(...sweetness);
   let right = sweetness.reduce((acc, curr) => acc + curr, 0);
 
   while (left < right) {
-    //* Mid represents the "maximum" sweetness that we want to test
-    let mid = left + ((right - left) >> 1);
+    //* `mid` represents the maximum sweetness each subarray can have
+    const mid = left + ((right - left) >> 1);
 
-    if (sweetnessValid(mid)) {
-      right = mid; //* Mid is a potential value, don't eliminate it
+    if (canSplit(mid)) {
+      right = mid; //* Potential candidate; don't eliminate from search space
     } else {
-      left = mid + 1; //* Need more sweetness, eliminate left
+      left = mid + 1; //* Need a larger sweetness value
     }
   }
 
+  //* The minimized maximum sweetness we will accept
   return left;
 }
 
@@ -53,8 +75,7 @@ console.log(divideChocolate([1, 2, 3, 4], 1)); //* 4
 console.log(divideChocolate([100], 1)); //* 1
 console.log(divideChocolate([100], 0)); //* 100
 
-//* Time: O(n log(m)) - "m" represents the SUM of sweetness
-//* The search space scales in size with the sum of the sweetness values
-//* Within the binary search O(log n), we call a function that takes O(n)
+//* Time: O(n log r) - Where `r` is the range of possible sweetness values [min(sweetness), sum(sweetness)]
+//* Within each while loop iteration (log r), we iterate over the entire array in the worst case (n)
 
-//* Space: O(1) - The space usage remains constant regardless of our input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
