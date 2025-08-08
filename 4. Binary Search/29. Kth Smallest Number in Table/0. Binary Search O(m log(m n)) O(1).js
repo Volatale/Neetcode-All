8db0f0fcdf969c5ebe38cae27f7e1c172215e4ec
volatale <nathanamarirose@gmail.com>
2025-08-09 00:45:00 +0100

    Update Kth Smallest Number in Table

--- 4. Binary Search/29. Kth Smallest Number in Table/0. Binary Search O(m log(m n)) O(1).js
+++ 4. Binary Search/29. Kth Smallest Number in Table/0. Binary Search O(m log(m n)) O(1).js
@@ -1,48 +1,90 @@
-//* Count the number of elements <= k
-//* Use the left and right bound as
-//* The matrix is supposed to be 1-indexed
-//* So instead of 0 to m * n - 1
-//* We do 1 to m * n; this represents our search space
-//* Mid represents the number of elements that need to be <= k
-//* At best, each row cannot have > n elements
-function kthSmallestNumberInTable(m, n, k) {
-  function tooManyLessThan(x) {
+//* We are given a matrix `mat` where mat[i][j] = i * j (1-indexed)
+//*     - mat[1][1] = 1
+//*     - mat[1][2] = 2
+//*     - mat[2][2] = 4, etc.
+//* The goal is to return the kth SMALLEST element in the multiplication table
+//* Some observations:
+//!     - The values in each row are monotonically increasing, as are the values in each column
+//*     - We also know we are searching for an element within the matrix itself
+//! Thus we can say the search space is the matrix itself
+//* Searching for the `kth` smallest element implies optimization
+//! With all of the above, we can likely apply binary search to optimize our searching
+//* We need to convert the 2D matrix index to a 1D representation
+//*     - row = Math.floor(index / COLS)
+//*     - col = index % COLS
+//* However, note that we are binary searching over the range of possible VALUES (elements), and not indices
+//*     - Why? We want the kth smallest number
+//* Thus, the range of possible values exists in the range [1, m * n]
+//*     - left = 1 (1 * 1)
+//*     - right = m * n
+//* For each iteration, we can ask "how many numbers in the table are <= x" (x = mid)
+//*     - Thus, mid allows us to find how many elements are <= mid
+//* If the count is < k, we need larger numbers
+//*     - left = mid + 1
+//* If the count >= k, we have found a potential candidate
+//*     - right = mid
+//! But how do we count how many elements are <= mid?
+//* In each row `i`, the first values are:
+//*     - i * 1, i * 2, i * 3, ..., i * n
+//*         - [1, ... ... ...]
+//*         - [2, ... ... ...]
+//*         - [3, ... ... ...]
+//*         - [4, ... ... ...]
+//* For each row `i`, the values in each column (j) increase by `i` each time
+//*     - [1, 2, 3] (i = 1)
+//*     - [2, 4, 6] (i = 2)
+//*     - [3, 6, 9] (i = 3)
+//* Thus, the number of elements <= mid in each row means "how many elements `j` such that `i * j <= mid`"
+//*     - (i * j <= mid). Isolate (solve for) `j`
+//*     - j <= mid / i
+//*         - Then take the floor of that
+//*     - The number of elements that can be <= mid per row is therefore `Math.min(mid / i, n)`
+//*         - If mid = 6 and i = 2, then (6 / 2 = 3 elements <= mid)
+//*         - In the "best" case, every element in the row (n elements) is <= mid
+//! Why are we doing this?
+//*     - We could sort every row in the matrix and return the `kth` index (-1)
+//*         - m rows, n columns, and it takes O(n log n) to sort, so O(mn * log(m * n)) time complexity
+//*     - If we do use binary search, iterate over the entire matrix and count the number of elements <= mid in each iteration
+//*         - But then we still have to iterate over the rows/columns to count
+//* We are using a mathematical formula to count the number of elements <= mid that exist per row
+//*     - Then, we sum the above results for each row
+//*     - Since its a formula, it can be computed in O(1) time
+function findKthNumber(m, n, k) {
+  function kGreaterThan(mid) {
+    //* Prefix sum of the count of elements <= mid in each row
     let count = 0;
 
-    //* For each row, x / i: That gives us the number of elements that "x" is less than
-    for (let i = 1; i <= m; i++) {
-      //* Take the minimum because we can't count more elements than we have columns
-      count += Math.min(Math.floor(x / i), n);
+    //* Iterate over each row and compute the number of elements <= mid
+    for (let i = 1; i <= m && count < k; i++) {
+      count += Math.min(Math.floor(mid / i), n);
     }
 
     return count >= k;
   }
 
-  //* 1 is the MINIMUM number in the table
-  //* m * n is the MAXIMUM number in the table
+  //* The range of possible values is in the range [1, m * n]
   let left = 1;
   let right = m * n;
 
   while (left < right) {
-    //* Mid = no. of elements we want to be less than mid
-    let mid = left + ((right - left) >> 1);
+    //* `mid` represents the no. of elements we'll allow to be <= mid
+    const mid = left + ((right - left) >> 1);
 
-    //* Check if there are <= k numbers < mid
-    if (tooManyLessThan(mid)) {
-      right = mid; //* Too many numbers >= mid
+    if (kGreaterThan(mid)) {
+      right = mid; //* Found potential candidate (don't eliminate from search space)
     } else {
-      left = mid + 1; //* Not enough numbers are less than "mid"
+      left = mid + 1; //* Need a larger value
     }
   }
 
+  //* Kth smallest element
   return left;
 }
 
-console.log(kthSmallestNumberInTable(3, 3, 5)); //* 3
-console.log(kthSmallestNumberInTable(2, 3, 6)); //* 6
+console.log(findKthNumber(3, 3, 5)); //* 3
+console.log(findKthNumber(2, 3, 6)); //* 6
 
-//* Time: O(m log(m * n))
-//* We do a binary search that scales with the number of rows and columns
-//* Then within each binary search, we do an O(m) for loop
+//* Time: O(m * log(m * n)) - There are `m` rows, and within each while loop iteration we perform O(m) work
+//* The range of possible values is given by [1, m * n]
 
-//* Time: O(1) - The space usage remains the same regardless of input size
+//* Space: O(1) - The memory usage remains constant regardless of input size
