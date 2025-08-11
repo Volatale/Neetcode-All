8dd0b9c072ee57f07faf1fc84e64f8355b4748f0
volatale <nathanamarirose@gmail.com>
2025-08-12 00:14:41 +0100

    Update Single Element in a Sorted Array

--- 4.5. Binary Search II/1. Single Element in a Sorted Array/0. Binary Search O(log n) O(1).js
+++ 4.5. Binary Search II/1. Single Element in a Sorted Array/0. Binary Search O(log n) O(1).js
@@ -1,54 +1,48 @@
-//! Sorted Array
-//*     - Consists of integers
-//*     - Every element appears TWICE, except for ONE that appears once
-//* There are (n - 1) elements with duplicates
-//* And there is only 1 extra
-//* For example: [1, 1, 2, 2, 3]
-//*     - The length of the array is 5
-//*     - (5 - 4) = 1 singular element
-//* Determine what side the singular element is on
-//*     - Case 1: The "mid" element is the singular
-//*     - Case 2: The singular element is on the left
-//*     - Case 3: The singular element is on the right
-//* If nums[mid] === nums[mid - 1]
-//*     - Then count how many elements exists on the left BEFORE mid - 1
-//* For example: [1, 2, 2, 3, 3]
-//*     - Mid would be 2 initially
-//*     - nums[mid] === nums[mid - 1]
-//*         - So excluding mid and mid - 1, there is 1 element on the left
-//! The number of elements on the left would THEREFORE BE ODD
-//* Thus, we know the number of elements on the RIGHT has to be EVEN
-//* Since there is only ONE element with a singular occurrences, it has to be on the ODD side
-//! We can use Binary Search since the array is sorted
-//*     - The search space is the array itself (since we are looking for an array element)
+//* We are given an int[] `nums` where every element appears TWICE, except for ONE (that appears once)
+//* Our goal is to return the single element
+//* Ultimately, this is a frequency-related problem
+//! However, we are told that the array is sorted
+//*     - In other words, the array exhibits monotonicity
+//* The array can be said to be sorted in non-decreasing order
+//*     - Thus, nums[i] <= nums[i + 1] <= ... <= nums[n - 1]
+//* We are searching for an element within the array
+//* And we know the array is sorted monotonically
+//! Therefore, we can likely apply binary search to optimize for time complexity
+//*     - The range of potential values is [nums[0], nums[n - 1]]
+//*     - The range of indices in the array is [0, n - 1]
+//*     - The values are sorted in non-monotonically decreasing order (duplicates exist)
+//! Mathematically speaking, the array is guaranteed to have an odd length
+//* There are `m` pairs, and one extra element
+//*     - Array length = n
+//*         - m = (nums.length - 1) / 2
+//*     - So the array length is 2n + 1
+//! The single element MUST exist at one of the even indices
+//* Within each iteration, we can determine which side contains MORE elements and travel in THAT direction
+//*     - But we must ensure we exclude the current element in our check
+//* If mid is ODD, then the left element CANNOT be out of bounds
+//* If mid is EVEN, then the right element CANNOT be out of bounds
 function singleNonDuplicate(nums) {
-  //* The array itself is the search space
+  //* The search space is our range of indices [0, n - 1]
   let left = 0;
   let right = nums.length - 1;
 
-  while (left <= right) {
-    //* The index we are searching to find the single element
+  while (left < right) {
+    //* `mid` represents the index of the element we (currently) think is the single
     const mid = left + ((right - left) >> 1);
 
-    //* If it !== the previous OR the next element, we found the single element
+    //* Found the single element (not equal to adjacent elements)
     if (
-      (mid - 1 < 0 || nums[mid - 1] !== nums[mid]) &&
-      (mid + 1 === nums.length || nums[mid] !== nums[mid + 1])
+      (mid & 1 && nums[mid - 1] !== nums[mid]) ||
+      (!(mid & 1) && nums[mid] !== nums[mid + 1])
     ) {
-      return nums[mid];
-    }
-
-    //* Determine how many elements exist on the left side of the array
-    const countOnLeft =
-      mid - 1 >= 0 && nums[mid - 1] === nums[mid] ? mid - 1 : mid;
-
-    //* Search the side with the odd number of elements
-    if (countOnLeft & 1) {
-      right = mid - 1;
+      right = mid; //* Found a potential candidate, or it exists on the left
     } else {
-      left = mid + 1;
+      left = mid + 1; //* The single element exists on the right portion
     }
   }
+
+  //* The single element
+  return nums[left];
 }
 
 console.log(singleNonDuplicate([1, 2, 2])); //* 1
@@ -57,6 +51,7 @@ console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8])); //* 2
 console.log(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11])); //* 10
 console.log(singleNonDuplicate([50, 51, 51, 52, 52])); //* 50
 console.log(singleNonDuplicate([0, 0, 1, 2, 2])); //* 1
+console.log(singleNonDuplicate([5])); //* 5
 
 //* Time: O(log n) - We eliminate half of the search space every iteration
 
