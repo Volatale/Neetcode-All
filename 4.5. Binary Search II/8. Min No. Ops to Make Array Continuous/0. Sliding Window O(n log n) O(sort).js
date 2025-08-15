29ab7bd3bf7c8f296b7b0c531eee025e005ed715
volatale <nathanamarirose@gmail.com>
2025-08-15 14:36:17 +0100

    Update Min No. Ops To Make Array Cont

--- 4.5. Binary Search II/8. Min No. Ops to Make Array Continuous/0. Sliding Window O(n log n) O(sort).js
+++ 4.5. Binary Search II/8. Min No. Ops to Make Array Continuous/0. Sliding Window O(n log n) O(sort).js
@@ -1,52 +1,67 @@
-//* A continous array can be defined as follows:
-//*     - All of the element is unique (frequency of 1)
-//*     - The difference between the max and minimum elements is equal to nums.length - 1
-//! Another way to word this is that, when sorted, nums follows the following formula:
-//*     - [x...x + n - 1]
-//*     - Where "x" is the minimum element
-//* For example, the following arrays are continuous:
-//*     - [1, 2, 3]
-//*     - As is [4, 5, 6, 7, 8]
-//! [1, 4, 5] is NOT continous
-//*     - It must follow the range [x...x + n - 1]
-//* In our case, we can apply a sliding window approach
-//*     - We need to REMOVE THE DUPLICATES from the nums array/
-//*         - Otherwise it causes complications later
-//*     - But before we do so, record the length of the unmodified array
-//* Maintain the sliding window invariant:
-//!     - A valid window has an END value of nums[start] + n - 1
-//* So keep expanding the window while this invariant is true
-//* Then, we can count the number of elements that DON'T exist in the window
-//*     - (end - start). We don't add 1 here since "end" is one index AHEAD of the valid window
-//* After every iteration, we increment "start" because we want to try with a new minimum
-//! This logic only works if we sort the array
-//*     - This is what allows us to check if the current window is valid or not
+//* We are given an int[] `nums` and are able to replace any element in nums with any number
+//* Nums is considered to be `continuous` if:
+//*     - All elements in nums are unique
+//*     - max(nums) - min(nums) === nums.length - 1
+//*         - [1, 2, 3] -> (3 - 1) === 2
+//*         - nums.length - 1 === 2
+//* We need to return the MINIMUM number of operations to make `nums` continuous
+//! Logically speaking, since we need continous elements, sorting the array makes sense
+//*     - This introduces a monotonic property to the array in case it didn't already have one
+//* The number of operations we may need to make is in the range [0, n - 1]
+//* Thus, our "search space" also exists within this range
+//! We need to precomute the exact range of values the array must end up with
+//*     - The elements in nums are in the range [x...x + n - 1]
+//*     - Where `x` is the minimum element in nums
+//* For example, if `x` = 4, and the array length is 4
+//*     - Then we need a continous array of [4...7]
+//*     - Which leaves us with [4, 5, 6, 7] in some ordering
+//! In a brute force manner, we can try "starting" with every possible starting point (value)
+//*     - Then, we just take the minimum no. of operations within each iteration
+//*     - Here, sorting makes no difference since we'd use a set to check for missing elements
+//! However, there is no real need to perform these checks in a brute force manner
+//*     - We can instead use a sliding window approach
+//* The sliding window invariant will enforce that the window holds elements in the range [x..x + n - 1]
+//*     - If this invariant is broken, then we know we need to swap some elements
+//*     - A valid window has an END value of nums[start] + n + 1
+//! Just like in the brute force version, we need to remove the duplicates from the array
+//*     - Otherwise we end up with complications
+//* Additionally (as in the brute force version)
+//*     - We want to try a new start value (x) within each iteration (outer loop)
+//*     - The goal is to compute the number of MISSING values that exist within each [start..end] range
+//! Essentially, for each `x` (nums[i]), we ensure that the END value exists within the current range
+//*     - If it does, we shift `end` and try to make it so that is NOT the case
+//* For example, if we have [1, 2, 3], then we don't have to perform ANY operations
+//*     - Why? Because the window invariant is never broken
+//*     - Thus, minOperations = Math.min(3, 3 - (3 - 0))
+//*         - n = 3 (because the length of the array is 3)
 function minOperations(nums) {
-  //* The length of the ORIGINAL array (before duplicates are removed)
-  const n = nums.length;
-  let operations = n; //* No. of operations needed does not exceed n
+  //* An array with a single element is continous ([x...x + n - 1])
+  if (nums.length === 1) return 0;
 
-  //* Remove the duplicates from the array, then sort in ascending order
+  const n = nums.length; //* Length of array BEFORE duplicate removals
+  let minOperations = n; //* No. of operations will never exceed n
+
+  //* Remove the duplicates from the array and then sort into ascending order
   nums = [...new Set(nums)].sort((a, b) => a - b);
 
   //* Marks the start and end of the sliding window
   let start = 0;
   let end = 0;
 
-  while (start < nums.length) {
-    //* Shift "end" while window is valid: valid range is [x...x + n + 1] (min, max)
+  while (end < nums.length) {
+    //* Keep shifting `end` until the sliding window constraint is BROKEN
     while (end < nums.length && nums[end] < nums[start] + n) {
       end++;
     }
 
-    //* The "end" pointer is NOT inclusive (we went 1 index BEYOND the valid index, so no + 1)
-    operations = Math.min(operations, n - (end - start));
-    start++; //* Try a new minimum value
+    minOperations = Math.min(minOperations, n - (end - start));
+    start++; //* Try a new starting point (x)
   }
 
-  return operations;
+  return minOperations;
 }
 
+console.log(minOperations([1, 2, 3, 4, 5])); //* 0
 console.log(minOperations([4, 2, 5, 3])); //* 0
 console.log(minOperations([1, 2, 3, 5, 6])); //* 1
 console.log(minOperations([1, 10, 100, 1000])); //* 3
@@ -54,6 +69,8 @@ console.log(minOperations([1, 1, 1])); //* 2
 console.log(minOperations([1, 1, 3])); //* 1
 console.log(minOperations([1, 5, 3, 2])); //* 1
 
-//* Time: O(n log n) - We have to sort the array, so the time taken scales with the time needed to sort
+//* Time: O(n log n) - The time taken scales with the time necessary to sort the array
+//* Additionally, regardless of the sorting algorithm used, we must iterate over the entire array
 
 //* Space: O(sort) - The memory usage scales with the sorting algorithm used
+//* In the best case, heap sort is used which results in O(1) space, but merge sort (O(n)) is more likely
--- 4.5. Binary Search II/8. Min No. Ops to Make Array Continuous/2. Brute Force O(n^2) O(n).js
+++ 4.5. Binary Search II/8. Min No. Ops to Make Array Continuous/2. Brute Force O(n^2) O(n).js
@@ -1,47 +1,46 @@
-//! TLE - Don't run it
-//* A continuous array can be thought of as:
-//*     - An array where (when nums is sorted) each successive element is 1 greater than the previous
-//* So for example, [1, 2, 3] is continuous
-//*     - As is [4, 6, 7, 5], because sorted, we get [4, 5, 6, 7]
-//* We have a RANGE of elements that start at the MINIMUM and end at the MAXIMUM
-//*     - The range in question is: [x...x + n - 1]
-//* If "x" = 4, and the array length is 4
-//*     - Then we get a continous array of [4...7]
-//* This checks our with the official definition of a continuous array
-//*     - Every element in unique (frequency of 1)
-//*     - The difference between the max and min is equal to nums.length - 1
-//* [4, 5, 6, 7]:
-//*     - Has all unique elements
-//*     - (7 - 4 = 3) === (4 - 1) === 3
-//* So in a brute force manner, we can try "starting" with EVERY distinct value in nums (x)
-//* Then, for each "x", we count how many elements in the range [x...x + n - 1] DON'T exist in nums
-//*     - The count tells us how many element we'd need to to change if we STARTED with "x"
-//* So we take the MINIMUM of all possible starting points of "x"
+//* We are given an int[] `nums` and are able to replace any element in nums with any number
+//* Nums is considered to be `continuous` if:
+//*     - All elements in nums are unique
+//*     - max(nums) - min(nums) === nums.length - 1
+//*         - [1, 2, 3] -> (3 - 1) === 2
+//*         - nums.length - 1 === 2
+//* We need to return the MINIMUM number of operations to make `nums` continuous
+//! Logically speaking, since we need continous elements, sorting the array makes sense
+//*     - This introduces a monotonic property to the array in case it didn't already have one
+//* The number of operations we may need to make is in the range [0, n - 1]
+//* Thus, our "search space" also exists within this range
+//! We need to precomute the exact range of values the array must end up with
+//*     - The elements in nums are in the range [x...x + n - 1]
+//*     - Where `x` is the minimum element in nums
+//* For example, if `x` = 4, and the array length is 4
+//*     - Then we need a continous array of [4...7]
+//*     - Which leaves us with [4, 5, 6, 7] in some ordering
+//! In a brute force manner, we can try "starting" with every possible starting point (value)
+//*     - Then, we just take the minimum
+//*     - Here, sorting makes no difference since we'd use a set to check for missing elements
 function minOperations(nums) {
-  //* An array with 1 element is continuous ([x...x + n - 1])
+  //* An array with a single element is continous ([x...x + n - 1])
   if (nums.length === 1) return 0;
 
   const n = nums.length;
-  const distinct = new Set(nums); //* For fast lookups to determine if "x" exists in the array
-  let operations = Infinity;
+  const distinct = new Set(nums); //* Get the distinct elements
+  let minOperations = Infinity;
 
-  //* Use every distinct element in nums to start the sequence [x...x + n - 1]
-  for (let x of distinct) {
-    //* No. of elements we have to CHANGE to make nums continous if we start with "x"
-    let missing = 0;
+  for (let i = 0; i < n; i++) {
+    let changes = 0;
 
-    //* Count how many elements in the range [x, x + n - 1] DON'T exist in nums
-    for (let num = x; num <= x + n - 1; num++) {
-      if (!distinct.has(num)) {
-        missing++;
+    //* Count how many elements in the range [x...x + n - 1] are missing
+    for (let x = nums[i]; x <= nums[i] + n - 1; x++) {
+      if (!distinct.has(x)) {
+        changes++;
       }
     }
 
-    //* Min no. of operations needed to make nums continuous
-    operations = Math.min(operations, missing);
+    //* Take the minimum no. of operations if possible
+    minOperations = Math.min(minOperations, changes);
   }
 
-  return operations;
+  return minOperations;
 }
 
 console.log(minOperations([4, 2, 5, 3])); //* 0
@@ -52,6 +51,6 @@ console.log(minOperations([1, 1, 3])); //* 1
 console.log(minOperations([1, 5, 3, 2])); //* 1
 
 //* Time: O(n^2) - In the worst case, every element in nums is unique
-//* So for each outer iterations (n), there are n inner iterations
+//* So for each outer iteration (n), there are n INNER iterations
 
-//* Space: O(n) - If every element in nums is unique, the set contains "n" elements
+//* Space: O(n) - The memory usage scales with the number of unique elements in nums (up to n)
